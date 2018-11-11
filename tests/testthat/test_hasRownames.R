9914569d2403df22baa692ae18848d37561b08ee
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-11 17:23:50 -0500

    Improve unit tests

---
+++ tests/testthat/test_hasRownames.R
@@ -0,0 +1,32 @@
+context("Has rownames?")
+
+as_tibble <- tibble::as_tibble
+
+
+
+test_that("assertHasRownames", {
+    df <- data.frame(
+        x = seq_len(2L),
+        y = seq_len(2L),
+        row.names = letters[seq_len(2L)]
+    )
+    expect_silent(
+        assertHasRownames(df)
+    )
+    expect_silent(
+        assertHasRownames(as(df, "DataFrame"))
+    )
+    expect_silent(
+        assertHasRownames(as_tibble(df, rownames = "rowname"))
+    )
+
+    expect_error(
+        assertHasRownames(DataFrame())
+    )
+    expect_error(
+        assertHasRownames(data.frame())
+    )
+    expect_error(
+        assertHasRownames(tibble())
+    )
+})
