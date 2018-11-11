d9c3afe6a91a5f22a2f523e3e4743d4eff6d38e1
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-11 17:48:39 -0500

    Improve assert check

--- tests/testthat/test_hasRownames.R
+++ tests/testthat/test_hasRownames.R
@@ -1,5 +1,6 @@
 context("Has rownames?")
 
+library(S4Vectors)
 as_tibble <- tibble::as_tibble
 
 test_that("assertHasRownames", {
@@ -8,23 +9,11 @@ test_that("assertHasRownames", {
         y = seq_len(2L),
         row.names = letters[seq_len(2L)]
     )
-    expect_silent(
-        assertHasRownames(df)
-    )
-    expect_silent(
-        assertHasRownames(as(df, "DataFrame"))
-    )
-    expect_silent(
-        assertHasRownames(as_tibble(df, rownames = "rowname"))
-    )
+    expect_silent(assertHasRownames(df))
+    expect_silent(assertHasRownames(as(df, "DataFrame")))
+    expect_silent(assertHasRownames(as_tibble(df, rownames = "rowname")))
 
-    expect_error(
-        assertHasRownames(DataFrame())
-    )
-    expect_error(
-        assertHasRownames(data.frame())
-    )
-    expect_error(
-        assertHasRownames(tibble())
-    )
+    expect_error(assertHasRownames(DataFrame()))
+    expect_error(assertHasRownames(data.frame()))
+    expect_error(assertHasRownames(tibble()))
 })
