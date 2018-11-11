f0276b228a27ce31b1fa0ee058f071927eb63f5d
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-11 17:26:09 -0500

    Improve unit test

---
+++ tests/testthat/test_isHeaderLevel.R
@@ -0,0 +1,11 @@
+context("Is Markdown header level?")
+
+
+
+test_that("assertIsHeaderLevel", {
+    expect_silent(assertIsHeaderLevel(1L))
+    expect_error(
+        object = assertIsHeaderLevel(8L),
+        regexp = "not a valid Markdown header level"
+    )
+})
