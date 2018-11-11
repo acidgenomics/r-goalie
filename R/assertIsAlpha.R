45331aefb5dabb69263db5ddba4fcf532a6412e3
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-11 11:12:08 -0500

    Use `x` instead of `object`

--- R/assertIsAlpha.R
+++ R/assertIsAlpha.R
@@ -7,11 +7,11 @@
 #'
 #' @examples
 #' assertIsAlpha(0.05)
-assertIsAlpha <- function(object) {
-    name <- get_name_in_parent(object)
+assertIsAlpha <- function(x) {
+    name <- get_name_in_parent(x)
     assert_that(
-        is_a_number(object),
-        is_in_open_range(object, lower = 0L, upper = 1L),
+        is_a_number(x),
+        is_in_open_range(x, lower = 0L, upper = 1L),
         msg = paste(name, "is not a numeric scalar between 0 and 1.")
     )
 }
