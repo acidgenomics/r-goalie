ab997d3238f09c472ff1a7fd00ee6b903fff1a26
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-11 17:47:00 -0500

    Update assert checks

--- tests/testthat/test_formals.R
+++ tests/testthat/test_formals.R
@@ -3,11 +3,7 @@ context("Formal arguments")
 test_that("assertFormalCompress", {
     expect_error(
         object = assertFormalCompress("XXX"),
-        regexp = paste(
-            "is_subset :",
-            "The element 'XXX' in object is not in",
-            "c\\(\"bzip2\", \"gzip\", \"xz\"\\)."
-        )
+        regexp = "gzip"
     )
     expect_silent(assertFormalCompress("xz"))
 })
