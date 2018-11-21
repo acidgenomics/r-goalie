2fc1cd9b088728507696c2f9fb96d6e991fe939b
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-21 17:04:17 +0000

    Fix documentation

--- R/package.R
+++ R/package.R
@@ -21,30 +21,8 @@
 #'   is_string
 "_PACKAGE"
 
-
-
 globalVariables(".")
 
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
-
-
-
-#' Parameters
-#'
-#' @name params
-#' @keywords internal
-#'
-#' @param envir `environment`. Environment.
-#' @param genes `character`. Genes.
-#' @param inherits `boolean`. Should the enclosing frames of the `environment`
-#'   be searched?
-#' @param names `character`. Names (e.g. rownames, colnames).
-#' @param x Object.
-#'
-#' @return
-#' - `boolean flag` for `are/has/is*()` functions.
-#' - Stop on error for `assert*()` functions
-#'   (see `base::stop()` or `base::stopifnot()` for details).
-NULL
