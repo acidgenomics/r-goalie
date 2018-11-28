ac09da864aa364429f1911a413784f51668287f4
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-28 13:11:18 -0500

    Improve documentation

--- R/deprecated.R
+++ R/deprecated.R
@@ -4,25 +4,19 @@
 
 
 #' Deprecated Functions
-#'
 #' @name deprecated
 #' @keywords internal
-#'
 #' @inheritParams params
-#'
-#' @return [.Deprecated()].
+#' @return `.Deprecated()`.
 NULL
 
 
 
 #' Defunct Functions
-#'
 #' @name defunct
 #' @keywords internal
-#'
 #' @inheritParams params
-#'
-#' @return [.Defunct()].
+#' @return `.Defunct()`.
 NULL
 
 
