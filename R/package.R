89a1c1ceee5e7768728f997fa82688990198ea0b
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-29 14:53:00 +0000

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -4,12 +4,14 @@
 #'
 #' @keywords internal
 #'
+#' @importFrom BiocGenerics rowSums
 #' @importFrom R.utils isDirectory isFile
+#' @importFrom SummarizedExperiment assay
 #' @importFrom assertive.base assert_are_identical
 #' @importFrom assertive.code is_existing
 #' @importFrom assertive.data assert_all_are_hex_colors is_hex_color
-#' @importFrom assertive.properties assert_has_dimnames assert_has_names
-#'   assert_is_non_empty has_colnames has_dimnames has_rownames
+#' @importFrom assertive.properties assert_has_dims assert_has_dimnames
+#'   assert_has_names assert_is_non_empty has_colnames has_dimnames has_rownames
 #' @importFrom assertive.sets assert_are_disjoint_sets assert_is_subset
 #' @importFrom assertive.types assert_is_a_bool assert_is_a_number
 #'   assert_is_a_string assert_is_all_of assert_is_an_integer assert_is_any_of
