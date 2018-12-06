db3c316498f39bfd48d6ccecfdf0cadc02c41d0e
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-06 09:56:44 -0500

    Move `assert_*()` imports to rexports, to reorganize

--- R/package.R
+++ R/package.R
@@ -7,16 +7,12 @@
 #' @importFrom BiocGenerics duplicated rowSums
 #' @importFrom R.utils isDirectory isFile
 #' @importFrom SummarizedExperiment assay
-#' @importFrom assertive.base assert_are_identical is2
+#' @importFrom assertive.base is2
 #' @importFrom assertive.code is_existing
-#' @importFrom assertive.data assert_all_are_hex_colors is_hex_color
-#' @importFrom assertive.properties assert_has_dims assert_has_dimnames
-#'   assert_has_names assert_is_non_empty has_colnames has_dimnames has_rownames
-#' @importFrom assertive.sets assert_are_disjoint_sets assert_is_subset
-#' @importFrom assertive.types assert_is_a_bool assert_is_a_number
-#'   assert_is_a_string assert_is_all_of assert_is_an_integer assert_is_any_of
-#'   assert_is_character
+#' @importFrom assertive.data is_hex_color
+#' @importFrom assertive.properties has_colnames has_dimnames has_rownames
 #' @importFrom assertthat on_failure<-
+#' @importFrom checkmate assert makeAssertion makeAssertionFunction
 #' @importFrom methods is setGeneric setMethod signature
 #' @importFrom utils globalVariables
 #' @importFrom rlang is_integerish is_scalar_double is_scalar_integerish
