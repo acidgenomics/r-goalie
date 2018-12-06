5dc7a00c2588c3332a72d7b1360ccfeb8c94bd33
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-06 12:37:33 -0500

    `checkAreNonExisting()`: Rework using checkmate approach

---
+++ R/checkAreNonExisting.R
@@ -0,0 +1,69 @@
+#' Does the Requested Variable Exist?
+#'
+#' @name checkAreNonExisting
+#' @aliases areNonExisting
+#' @inherit params
+#'
+#' @param x `character`. Variable names to check in `environment`.
+#'
+#' @examples
+#' areNonExisting(c("XXX", "YYY"))
+NULL
+
+
+
+.areNonExisting <- function(
+    x,
+    envir = parent.frame(),
+    inherits = TRUE
+) {
+    all(!is_existing(x, envir = envir, inherits = inherits))
+}
+
+
+
+#' @rdname checkAreNonExisting
+#' @export
+checkAreNonExisting <- function(x) {
+    if (isTRUE(.areNonExisting(x))) {
+        TRUE
+    } else {
+        "Detected variables that already exist in the environment"
+    }
+}
+
+
+
+#' @rdname checkAreNonExisting
+#' @export
+check_are_non_existing <- checkAreNonExisting
+
+
+
+#' @rdname checkAreNonExisting
+#' @export
+assertAreNonExisting <- makeAssertionFunction(checkAreNonExisting)
+
+
+
+#' @rdname checkAreNonExisting
+#' @export
+testAreNonExisting <- makeTestFunction(checkAreNonExisting)
+
+
+
+#' @rdname checkAreNonExisting
+#' @export
+test_are_non_existing <- testAreNonExisting
+
+
+
+#' @rdname checkAreNonExisting
+#' @export
+assert_are_non_existing <- assertAreNonExisting
+
+
+
+#' @rdname checkAreNonExisting
+#' @export
+expect_are_non_existing <- makeExpectationFunction(checkAreNonExisting)
