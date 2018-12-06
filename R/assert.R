89315b9adac50c5c03901ec96197cfa239046b6f
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-06 09:35:34 -0500

    Reexport `checkmate::assert()` here

--- R/assert.R
+++ R/assert.R
@@ -1,21 +1,3 @@
-#' Assert That Certain Conditions Are True
-#'
-#' `assert()` behaves like `stopifnot()` but supports more informative error
-#' messages. The function supports multiple individual assertions must return a
-#' `boolean flag` (i.e. a single `TRUE` or `FALSE` `logical`).
-#'
-#' @importFrom assertthat assert_that
-#' @inheritParams assertthat::assert_that
-#' @inheritParams params
+#' @importFrom checkmate assert
 #' @export
-#'
-#' @seealso
-#' - `stopifnot()`.
-#' - `assertthat::assert_that()`.
-#'
-#' @examples
-#' assert(
-#'     is.atomic("example"),
-#'     is.character("example")
-#' )
-assertthat::assert_that -> assert
+checkmate::assert
