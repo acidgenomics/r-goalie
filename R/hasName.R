f35ff0c647884a162d91b179adb45900f2c9ee92
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-06 10:42:19 -0500

    `hasName()`: Add `assertHasName()` and snake variants

--- R/hasName.R
+++ R/hasName.R
@@ -1,5 +1,28 @@
 #' Does an Object Have an Element with This Name?
-#' @importFrom rlang has_name
+#'
+#' @name hasName
 #' @inheritParams params
+#'
 #' @export
-rlang::has_name -> hasName
+NULL
+
+
+
+#' @rdname hasName
+#' @export
+hasName <- has_name
+
+#' @importFrom rlang has_name
+#' @usage NULL
+#' @export
+rlang::has_name
+
+
+
+#' @rdname hasName
+#' @export
+assertHasName <- makeAssertionFunction(hasName)
+
+#' @rdname hasName
+#' @export
+assert_has_name <- assertHasName
