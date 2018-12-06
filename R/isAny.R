7a2c9a5c8c726be03aa620d5e72fdf382da376cf
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-06 09:56:31 -0500

    Use `classes` instead of `class` for `isAny()`

--- R/isAny.R
+++ R/isAny.R
@@ -1,6 +1,6 @@
 #' Does an Object Belong to Any of These Classes?
 #' @inheritParams params
 #' @export
-isAny <- function(x, class) {
-    any(is2(x = x, class = class))
+isAny <- function(x, classes) {
+    any(is2(x = x, class = classes))
 }
