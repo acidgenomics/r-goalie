2889846f3464b350a9f4d000f2fdbf1631c343f1
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-07 15:40:07 -0500

    Update documentation

--- R/params.R
+++ R/params.R
@@ -8,16 +8,20 @@
 #' @param classes `character`. Object classes.
 #' @param envir `environment`. Environment.
 #' @param genes `character`. Genes.
+#' @param info `character`. Extra information to be included in the message for
+#'   the testthat reporter. See `testthat::expect_that()`.
 #' @param inherits `boolean`. Should the enclosing frames of the `environment`
 #'   be searched?
-#' @param name `string`. Name.
-#' @param names `character`. Names (e.g. rownames, colnames).
+#' @param name `character`. Element name.
+#' @param names `character`. Element names (e.g. row names, column names).
 #' @param x Object.
 #' @param .var.name `string`. Name of the checked object to print in assertions.
 #' @param ... Additional arguments.
 #'
 #' @seealso
+#' - `checkmate::makeTestFunction()`.
 #' - `checkmate::makeAssertionFunction()`.
+#' - `checkmate::makeExpectationFunction()`.
 #'
 #' @return
 #' - `boolean flag` for `are/has/is*()` functions.
