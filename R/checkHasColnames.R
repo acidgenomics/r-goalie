a501faefd310aed4b57df2328b46f6bd7a69a552
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-07 15:39:24 -0500

    `checkHasColnames()`: Initial commit

---
+++ R/checkHasColnames.R
@@ -0,0 +1,74 @@
+#' Does the Object Have Column Names?
+#'
+#' @name checkHasColnames
+#' @inherit params
+#'
+#' @examples
+#' ## Pass ====
+#' x <- data.frame(
+#'     "sample1" = c(1L, 2L),
+#'     "sample2" = c(3L, 4L),
+#'     row.names = c("gene1", "gene2")
+#' )
+#' print(x)
+#' checkHasColnames(x)
+#'
+#' ## Fail ====
+#' x <- data.frame()
+#' print(x)
+#' colnames(x)
+#' checkHasColnames(x)
+#'
+#' x <- S4Vectors::DataFrame()
+#' print(x)
+#' colnames(x)
+#' checkHasColnames(x)
+NULL
+
+
+
+#' @rdname checkHasColnames
+#' @export
+checkHasColnames <- function(x) {
+    ok <- length(colnames(x)) > 0L
+    if (!ok) {
+        return("Object does not have column names")
+    }
+    TRUE
+}
+
+
+
+#' @rdname checkHasColnames
+#' @export
+check_has_colnames <- checkHasColnames
+
+
+
+#' @rdname checkHasColnames
+#' @export
+testHasColnames <- makeTestFunction(checkHasColnames)
+
+
+
+#' @rdname checkHasColnames
+#' @export
+test_has_colnames <- testHasColnames
+
+
+
+#' @rdname checkHasColnames
+#' @export
+assertHasColnames <- makeAssertionFunction(checkHasColnames)
+
+
+
+#' @rdname checkHasColnames
+#' @export
+assert_has_colnames <- assertHasColnames
+
+
+
+#' @rdname checkHasColnames
+#' @export
+expect_has_colnames <- makeExpectationFunction(checkHasColnames)
