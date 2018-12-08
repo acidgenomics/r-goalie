16a034fd25cd0bb98dfadd965ec2b295391a2f81
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-08 16:24:01 -0500

    Start over on unit testing

--- tests/testthat/test_formals.R
+++
@@ -1,9 +0,0 @@
-context("Formal arguments")
-
-test_that("assertFormalCompress", {
-    expect_error(
-        object = assertFormalCompress("XXX"),
-        regexp = "gzip"
-    )
-    expect_silent(assertFormalCompress("xz"))
-})
