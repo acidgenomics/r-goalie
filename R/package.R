4f936bff37c5f8bdc44c838af9f2f4de8e3fe237
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-08 08:52:13 -0500

    Update the imports

--- R/package.R
+++ R/package.R
@@ -1,8 +1,3 @@
-# FIXME Use checkmate variants instead of rlang where possible?
-# (e.g. is_integerish, is_scalar_double, is_string)
-
-
-
 #' goalie
 #'
 #' Assertive check functions for defensive R programming.
@@ -10,22 +5,31 @@
 #' @keywords internal
 #'
 #' @importFrom BiocGenerics duplicated rowSums
-#' @importFrom R.utils isDirectory isFile
+#' @importFrom R.utils capitalize
 #' @importFrom assertive.base is2
 #' @importFrom assertive.code is_existing
 #' @importFrom assertive.data is_hex_color
 #' @importFrom assertive.properties has_colnames has_dimnames
 #' @importFrom assertthat on_failure<-
-#' @importFrom checkmate assert assertClass assertLogical assertNames
-#'   makeAssertion makeAssertionFunction makeExpectation makeExpectationFunction
-#'   makeTest makeTestFunction testClass testFlag testScalar testString vname
+#' @importFrom checkmate assert assertClass assertFlag assertLogical assertNames
+#'   checkClass makeAssertion makeAssertionFunction makeExpectation
+#'   makeExpectationFunction makeTest makeTestFunction testClass testFlag
+#'   testScalar testString vname
 #' @importFrom methods as is setGeneric setMethod signature
 #' @importFrom utils globalVariables
-#' @importFrom rlang is_integerish is_scalar_double
-#'   is_scalar_integerish is_string
 "_PACKAGE"
 
 globalVariables(".")
+
+# Remove this in a future update, in favor of `assert()`.
+#' @importFrom assertthat assert_that
+#' @export
+assertthat::assert_that
+
+# Remove this in a future update, in favor of `validate()`.
+#' @importFrom assertthat validate_that
+#' @export
+assertthat::validate_that
 
 #' @importFrom magrittr %>%
 #' @export
