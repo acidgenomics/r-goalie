76d23060fb463023d7edd6ace49bade93f9d6613
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 08:02:15 -0500

    Reexport `assertHasNames()`

---
+++ R/assertive-assertHasNames.R
@@ -0,0 +1,19 @@
+#' @importFrom assertive.properties has_names
+#' @export
+assertive::has_names
+
+#' @rdname reexports
+#' @usage NULL
+#' @export
+hasNames <- has_names
+
+
+
+#' @importFrom assertive.properties assert_has_names
+#' @export
+assertive::assert_has_names
+
+#' @rdname reexports
+#' @usage NULL
+#' @export
+assertHasNames <- assert_has_names
