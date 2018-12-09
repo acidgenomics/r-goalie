beaa66716ba340900e3a0273f408121df4ee444e
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 10:41:00 -0500

    Export `assertAllAreExisting()`

---
+++ R/assertive-assertAllAreExisting.R
@@ -0,0 +1,8 @@
+#' @importFrom assertive.code assert_all_are_existing
+#' @export
+assertive::assert_all_are_existing
+
+#' @rdname reexports
+#' @usage NULL
+#' @export
+assertAllAreExisting <- assert_all_are_existing
