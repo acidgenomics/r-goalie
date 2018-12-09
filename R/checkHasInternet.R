d1b22bc4335c2456577b7536c8387ae50560fdf9
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-08 21:22:11 -0500

    `checkHasInternet()`: Initial commit

---
+++ R/checkHasInternet.R
@@ -0,0 +1,49 @@
+#' Does the Current Session Have an Internet Connection?
+#'
+#' @aliases hasInternet has_internet
+#' @inherit params
+#' @export
+#'
+#' @examples
+#' checkHasInternet()
+checkHasInternet <- function() {
+    ok <- has_internet()
+    if (!ok) {
+        return("Internet connection failure")
+    }
+    TRUE
+}
+
+
+
+#' @rdname checkHasInternet
+#' @export
+check_has_internet <- checkHasInternet  # nolint
+
+
+
+#' @rdname checkHasInternet
+#' @export
+testHasInternet <- function() {
+    isTRUE(checkHasInternet())
+}
+
+
+
+#' @rdname checkHasInternet
+#' @export
+test_has_internet <- testHasInternet
+
+
+
+#' @rdname checkHasInternet
+#' @export
+assertHasInternet <- function() {
+    assert(checkHasInternet())
+}
+
+
+
+#' @rdname checkHasInternet
+#' @export
+assert_has_internet <- assertHasInternet
