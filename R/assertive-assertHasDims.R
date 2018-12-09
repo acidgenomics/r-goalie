502a0b5c10b7ec9b611398f43a62d1a346d8dece
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 10:14:46 -0500

    Reexport `assertHasDims()`

---
+++ R/assertive-assertHasDims.R
@@ -0,0 +1,19 @@
+#' @importFrom assertive.properties has_dims
+#' @export
+assertive.properties::has_dims
+
+#' @rdname reexports
+#' @usage NULL
+#' @export
+hasDims <- has_dims
+
+
+
+#' @importFrom assertive.properties assert_has_dims
+#' @export
+assertive.properties::assert_has_dims
+
+#' @rdname reexports
+#' @usage NULL
+#' @export
+assertHasDims <- assert_has_dims
