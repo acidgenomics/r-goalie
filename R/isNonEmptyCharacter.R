2201d3731fb681e070c5eb023f9e00fc0979a420
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 17:42:08 -0500

    `isCharacter()`: Stricter character checker that returns FALSE if there are NA, empty, or missing strings

---
+++ R/isNonEmptyCharacter.R
@@ -0,0 +1,36 @@
+#' Does the Argument Contain a (Non-Empty) Character?
+#'
+#' @section Enforced parameters:
+#'
+#' - Must have length.
+#' - Cannot contain `NA` strings.
+#' - Cannot contain empty (`""`) strings.
+#'
+#' @name isCharacter
+#' @inherit params
+#'
+#' @examples
+#' ## Pass ====
+#' isCharacter("a")
+#'
+#' ## Fail ====
+#' isCharacter(NULL)
+#' isCharacter(character())
+#' isCharacter("")
+#' isCharacter(NA_character_)
+NULL
+
+
+
+#' @rdname isCharacter
+#' @importFrom assertive.strings is_non_missing_nor_empty_character
+#' @export
+isCharacter <- function(x) {
+    if (!is(x, "character")) {
+        return(FALSE)
+    }
+    if (length(x) == 0L) {
+        return(FALSE)
+    }
+    all(is_non_missing_nor_empty_character(x))
+}
