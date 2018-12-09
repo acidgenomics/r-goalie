dcf2ab6aec4d5147d1c3380c23d750b55b6da2da
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 07:00:28 -0500

    Reexport `checkEnvironment()`

---
+++ R/checkEnvironment.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkEnvironment
+#' @export
+checkmate::checkEnvironment
+
+#' @importFrom checkmate check_environment
+#' @export
+checkmate::check_environment
+
+
+
+#' @importFrom checkmate testEnvironment
+#' @export
+checkmate::testEnvironment
+
+#' @importFrom checkmate test_environment
+#' @export
+checkmate::test_environment
+
+
+
+#' @importFrom checkmate assertEnvironment
+#' @export
+checkmate::assertEnvironment
+
+#' @importFrom checkmate assert_environment
+#' @export
+checkmate::assert_environment
+
+
+
+#' @importFrom checkmate expect_environment
+#' @export
+checkmate::expect_environment
