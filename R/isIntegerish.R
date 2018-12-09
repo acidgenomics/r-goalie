9636fa27cf798893f0cb7303d2b7e50c646a9881
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 18:13:32 -0500

    Consolidate scalar functions

--- R/isIntegerish.R
+++ R/isIntegerish.R
@@ -9,18 +9,12 @@
 #'
 #' @seealso
 #' - `rlang::is_integerish()`.
-#' - `rlang::is_scalar_integer()`.
-#' - `rlang::is_scalar_integerish()`.
 #' - `checkmate::checkIntegerish`.
-#' - `checkmate::checkInt`.
 #' - `assertive::is_a_number`.
 #'
 #' @examples
+#' isIntegerish(seq_len(2L))
 #' isIntegerish(c(1, 2))
-#'
-#' ## Requires scalar integerish.
-#' isInt(1)
-#' isInt(1L)
 NULL
 
 
@@ -29,10 +23,3 @@ NULL
 #' @importFrom rlang is_integerish
 #' @export
 isIntegerish <- is_integerish
-
-
-
-#' @describeIn isIntegerish Short alias for `isScalarIntegerish`.
-#' @importFrom rlang is_scalar_integerish
-#' @export
-isInt <- is_scalar_integerish
--- R/isScalar.R
+++ R/isScalar.R
@@ -24,7 +24,38 @@ NULL
 #' @rdname isScalar
 #' @importFrom checkmate testScalar
 #' @export
-isScalar <- testScalar
+isScalar <- function(x) {
+    testScalar(x)
+}
+
+
+
+#' @rdname isScalar
+#' @importFrom rlang is_scalar_list
+#' @export
+isScalarList <- is_scalar_list
+
+
+
+#' @rdname isScalar
+#' @importFrom rlang is_scalar_atomic
+#' @export
+isScalarAtomic <- is_scalar_atomic
+
+
+
+#' @rdname isScalar
+#' @importFrom rlang is_scalar_vector
+#' @export
+isScalarVector <- is_scalar_vector
+
+
+
+#' @rdname isScalar
+#' @export
+isScalarNumeric <- function(x) {
+    isScalar(x) && is(x, "numeric")
+}
 
 
 
@@ -39,3 +70,49 @@ isScalarInteger <- is_scalar_integer
 #' @importFrom rlang is_scalar_integerish
 #' @export
 isScalarIntegerish <- is_scalar_integerish
+
+
+
+#' @rdname isScalar
+#' @importFrom rlang is_scalar_double
+#' @export
+isScalarDouble <- is_scalar_double
+
+
+
+#' @rdname isScalar
+#' @importFrom rlang is_scalar_character
+#' @export
+isScalarCharacter <- is_scalar_character
+
+
+
+#' @rdname isScalar
+#' @importFrom rlang is_scalar_logical
+#' @export
+isScalarLogical <- is_scalar_logical
+
+
+
+#' @describeIn isScalar Alias for `isScalarCharacter`.
+#' @importFrom rlang is_string
+#' @export
+isString <- is_string
+
+
+
+#' @describeIn isScalar Alias for `isScalarLogical`.
+#' @export
+isFlag <- isScalarLogical
+
+
+
+#' @describeIn isScalar Alias for `isScalarNumeric`.
+#' @export
+isNumber <- isScalarNumeric
+
+
+
+#' @describeIn isScalar Alias for `isScalarIntegerish`.
+#' @export
+isInt <- isScalarIntegerish
