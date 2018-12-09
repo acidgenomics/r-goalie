a446fd12b72bc0d4182aee8d1fedd679311643f9
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 07:47:03 -0500

    Reexport `assertAreDisjointSets()`

---
+++ R/assertAreDisjointSets.R
@@ -0,0 +1,19 @@
+#' @importFrom assertive.sets are_disjoint_sets
+#' @export
+assertive::are_disjoint_sets
+
+#' @rdname reexports
+#' @usage NULL
+#' @export
+areDisjointSets <- are_disjoint_sets
+
+
+
+#' @importFrom assertive.sets assert_are_disjoint_sets
+#' @export
+assertive::assert_are_disjoint_sets
+
+#' @rdname reexports
+#' @usage NULL
+#' @export
+assertAreDisjointSets <- assert_are_disjoint_sets
