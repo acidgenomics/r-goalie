0793af5b8ae9c7881ebb73e7554cfd89a2c290b9
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-08 21:07:19 -0500

    Simplify `validate()` export method

--- R/reexports-assertthat.R
+++ R/reexports-assertthat.R
@@ -2,6 +2,12 @@
 #' @export
 assertthat::assert_that
 
+
+
 #' @importFrom assertthat validate_that
 #' @export
 assertthat::validate_that
+
+#' @describeIn reexports Shorter name variant of `validate_that`.
+#' @export
+validate <- validate_that
