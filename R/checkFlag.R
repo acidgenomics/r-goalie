ab82abd9803279722588778ec66660276b5b88b0
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 07:06:09 -0500

    Reexport `checkFlag()`

---
+++ R/checkFlag.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkFlag
+#' @export
+checkmate::checkFlag
+
+#' @importFrom checkmate check_flag
+#' @export
+checkmate::check_flag
+
+
+
+#' @importFrom checkmate testFlag
+#' @export
+checkmate::testFlag
+
+#' @importFrom checkmate test_flag
+#' @export
+checkmate::test_flag
+
+
+
+#' @importFrom checkmate assertFlag
+#' @export
+checkmate::assertFlag
+
+#' @importFrom checkmate assert_flag
+#' @export
+checkmate::assert_flag
+
+
+
+#' @importFrom checkmate expect_flag
+#' @export
+checkmate::expect_flag
