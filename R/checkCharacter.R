57f3c97e4bf0d8f83331678910d9abe904581569
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 06:59:33 -0500

    Reexport `checkCharacter()`

---
+++ R/checkCharacter.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkCharacter
+#' @export
+checkmate::checkCharacter
+
+#' @importFrom checkmate check_character
+#' @export
+checkmate::check_character
+
+
+
+#' @importFrom checkmate testCharacter
+#' @export
+checkmate::testCharacter
+
+#' @importFrom checkmate test_character
+#' @export
+checkmate::test_character
+
+
+
+#' @importFrom checkmate assertCharacter
+#' @export
+checkmate::assertCharacter
+
+#' @importFrom checkmate assert_character
+#' @export
+checkmate::assert_character
+
+
+
+#' @importFrom checkmate expect_character
+#' @export
+checkmate::expect_character
