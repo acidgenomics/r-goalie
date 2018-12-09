8c86c5b97516ecb4a2cc35d2f1c722518e3bc19b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 07:01:36 -0500

    Reexport `checkDirectoryExists()`

---
+++ R/checkDirectoryExists.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkDirectoryExists
+#' @export
+checkmate::checkDirectoryExists
+
+#' @importFrom checkmate check_directory_exists
+#' @export
+checkmate::check_directory_exists
+
+
+
+#' @importFrom checkmate testDirectoryExists
+#' @export
+checkmate::testDirectoryExists
+
+#' @importFrom checkmate test_directory_exists
+#' @export
+checkmate::test_directory_exists
+
+
+
+#' @importFrom checkmate assertDirectoryExists
+#' @export
+checkmate::assertDirectoryExists
+
+#' @importFrom checkmate assert_directory_exists
+#' @export
+checkmate::assert_directory_exists
+
+
+
+#' @importFrom checkmate expect_directory_exists
+#' @export
+checkmate::expect_directory_exists
