6495b6e9945912a31f7d5401c0b1c293453a3431
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 10:54:54 -0500

    Update NAMESPACE

--- R/assertive-assertAreDisjointSets.R
+++ R/assertive-assertAreDisjointSets.R
@@ -1,6 +1,6 @@
-#' @importFrom assertive are_disjoint_sets
+#' @importFrom assertive.sets are_disjoint_sets
 #' @export
-assertive::are_disjoint_sets
+assertive.sets::are_disjoint_sets
 
 #' @rdname reexports
 #' @usage NULL
@@ -9,9 +9,9 @@ areDisjointSets <- are_disjoint_sets
 
 
 
-#' @importFrom assertive assert_are_disjoint_sets
+#' @importFrom assertive.sets assert_are_disjoint_sets
 #' @export
-assertive::assert_are_disjoint_sets
+assertive.sets::assert_are_disjoint_sets
 
 #' @rdname reexports
 #' @usage NULL
