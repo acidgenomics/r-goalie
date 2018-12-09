2160035c7f3f3313eed60571cadd67dc84ecfb6b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 08:06:36 -0500

    Reexport `checkFunction()`

---
+++ R/checkmate-checkFunction.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkFunction
+#' @export
+checkmate::checkFunction
+
+#' @importFrom checkmate check_function
+#' @export
+checkmate::check_function
+
+
+
+#' @importFrom checkmate testFunction
+#' @export
+checkmate::testFunction
+
+#' @importFrom checkmate test_function
+#' @export
+checkmate::test_function
+
+
+
+#' @importFrom checkmate assertFunction
+#' @export
+checkmate::assertFunction
+
+#' @importFrom checkmate assert_function
+#' @export
+checkmate::assert_function
+
+
+
+#' @importFrom checkmate expect_function
+#' @export
+checkmate::expect_function
