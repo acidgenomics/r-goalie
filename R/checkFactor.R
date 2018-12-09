5f26d65aa93cb1ebdbeda37b462a654f9f4c4d77
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 06:59:20 -0500

    Reexport `checkFactor()`

---
+++ R/checkFactor.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkFactor
+#' @export
+checkmate::checkFactor
+
+#' @importFrom checkmate check_factor
+#' @export
+checkmate::check_factor
+
+
+
+#' @importFrom checkmate testFactor
+#' @export
+checkmate::testFactor
+
+#' @importFrom checkmate test_factor
+#' @export
+checkmate::test_factor
+
+
+
+#' @importFrom checkmate assertFactor
+#' @export
+checkmate::assertFactor
+
+#' @importFrom checkmate assert_factor
+#' @export
+checkmate::assert_factor
+
+
+
+#' @importFrom checkmate expect_factor
+#' @export
+checkmate::expect_factor
