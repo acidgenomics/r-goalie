827e84152fc9a79a18f62df32cdb81abf07ce6cc
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 13:01:09 -0500

    `isExisting()`: Initial commit

---
+++ R/isExisting.R
@@ -0,0 +1,18 @@
+#' Is Existing?
+#' @name isExisting
+#' @inherit params
+NULL
+
+
+
+#' @rdname isExisting
+#' @importFrom assertive.code is_existing
+#' @export
+isExisting <- assertive.code::is_existing
+
+
+
+#' @rdname isExisting
+#' @importFrom assertive.code assert_all_are_existing
+#' @export
+assertAllAreExisting <- assertive.code::assert_all_are_existing
