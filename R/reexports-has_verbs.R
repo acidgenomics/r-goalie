a6ed42192f7727157a76d0b2188098202e27c054
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-08 20:56:48 -0500

    Reorganize reexports

---
+++ R/reexports-has_verbs.R
@@ -0,0 +1,142 @@
+# hasColnames ==================================================================
+# Already exporting assertHasColnames.
+
+#' @rdname checkHasColnames
+#' @export
+hasColnames <- testHasColnames
+
+#' @rdname checkHasColnames
+#' @export
+has_colnames <- hasColnames  # nolint
+
+
+
+# hasCols ======================================================================
+#' @importFrom assertive.properties assert_has_cols
+#' @export
+assertive.properties::assert_has_cols
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+assertHasCols <- assert_has_cols
+
+#' @importFrom assertive.properties has_cols
+#' @export
+assertive.properties::has_cols
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+hasCols <- has_cols
+
+
+
+# hasDimnames ==================================================================
+#' @importFrom assertive.properties assert_has_dimnames
+#' @export
+assertive.properties::assert_has_dimnames
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+assertHasDimnames <- assert_has_dimnames
+
+#' @importFrom assertive.properties has_dimnames
+#' @export
+assertive.properties::has_dimnames
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+hasDimnames <- has_dimnames
+
+
+
+# hasDims ======================================================================
+#' @importFrom assertive.properties assert_has_dims
+#' @export
+assertive.properties::assert_has_dims
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+assertHasDims <- assert_has_dims
+
+#' @importFrom assertive.properties has_dims
+#' @export
+assertive.properties::has_dims
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+hasDims <- has_dims
+
+
+
+# hasName ======================================================================
+# @seealso rlang::has_name
+
+#' @importFrom utils hasName
+#' @export
+utils::hasName
+
+#' @describeIn reexports snake alias.
+#' @usage NULL
+#' @export
+has_name <- hasName  # nolint
+
+
+
+# hasNames =====================================================================
+#' @importFrom assertive.properties assert_has_names
+#' @export
+assertive.properties::assert_has_names
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+assertHasNames <- assert_has_names
+
+#' @importFrom assertive.properties has_names
+#' @export
+assertive.properties::has_names
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+hasNames <- has_names
+
+
+
+# hasRownames ==================================================================
+# Already exporting assertHasRownames.
+
+#' @rdname checkHasRownames
+#' @export
+hasRownames <- testHasRownames
+
+#' @rdname checkHasRownames
+#' @export
+has_rownames <- hasRownames  # nolint
+
+
+
+# hasRows ======================================================================
+#' @importFrom assertive.properties assert_has_rows
+#' @export
+assertive.properties::assert_has_rows
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+assertHasRows <- assert_has_rows
+
+#' @importFrom assertive.properties has_rows
+#' @export
+assertive.properties::has_rows
+
+#' @describeIn reexports camel alias.
+#' @usage NULL
+#' @export
+hasRows <- has_rows
