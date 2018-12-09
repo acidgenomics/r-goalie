b94fcd0596f08d15bdbdaba26ab025ea11c3de2b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 07:19:52 -0500

    Reexport `checkSetEqual()`

---
+++ R/checkSetEqual.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkSetEqual
+#' @export
+checkmate::checkSetEqual
+
+#' @importFrom checkmate check_set_equal
+#' @export
+checkmate::check_set_equal
+
+
+
+#' @importFrom checkmate testSetEqual
+#' @export
+checkmate::testSetEqual
+
+#' @importFrom checkmate test_set_equal
+#' @export
+checkmate::test_set_equal
+
+
+
+#' @importFrom checkmate assertSetEqual
+#' @export
+checkmate::assertSetEqual
+
+#' @importFrom checkmate assert_set_equal
+#' @export
+checkmate::assert_set_equal
+
+
+
+#' @importFrom checkmate expect_set_equal
+#' @export
+checkmate::expect_set_equal
