92e960069e9955d22b2cf5b2c35279ce96eef558
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 08:14:46 -0500

    Reexport `checkInt()` and `checkInteger()`

---
+++ R/checkmate-checkInteger.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkInteger
+#' @export
+checkmate::checkInteger
+
+#' @importFrom checkmate check_integer
+#' @export
+checkmate::check_integer
+
+
+
+#' @importFrom checkmate testInteger
+#' @export
+checkmate::testInteger
+
+#' @importFrom checkmate test_integer
+#' @export
+checkmate::test_integer
+
+
+
+#' @importFrom checkmate assertInteger
+#' @export
+checkmate::assertInteger
+
+#' @importFrom checkmate assert_integer
+#' @export
+checkmate::assert_integer
+
+
+
+#' @importFrom checkmate expect_integer
+#' @export
+checkmate::expect_integer
