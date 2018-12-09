86dfd18cf1930eeac6e58a17a46cacd7e7a16764
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 07:04:11 -0500

    Reexport `checkDataFrame()`

---
+++ R/checkDataFrame.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkDataFrame
+#' @export
+checkmate::checkDataFrame
+
+#' @importFrom checkmate check_data_frame
+#' @export
+checkmate::check_data_frame
+
+
+
+#' @importFrom checkmate testDataFrame
+#' @export
+checkmate::testDataFrame
+
+#' @importFrom checkmate test_data_frame
+#' @export
+checkmate::test_data_frame
+
+
+
+#' @importFrom checkmate assertDataFrame
+#' @export
+checkmate::assertDataFrame
+
+#' @importFrom checkmate assert_data_frame
+#' @export
+checkmate::assert_data_frame
+
+
+
+#' @importFrom checkmate expect_data_frame
+#' @export
+checkmate::expect_data_frame
