a0b1fe91fc20c3b72ba5d3a85b1815ce0a8d6ab3
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 07:01:22 -0500

    Reexport `checkClass()`

---
+++ R/checkClass.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkClass
+#' @export
+checkmate::checkClass
+
+#' @importFrom checkmate check_class
+#' @export
+checkmate::check_class
+
+
+
+#' @importFrom checkmate testClass
+#' @export
+checkmate::testClass
+
+#' @importFrom checkmate test_class
+#' @export
+checkmate::test_class
+
+
+
+#' @importFrom checkmate assertClass
+#' @export
+checkmate::assertClass
+
+#' @importFrom checkmate assert_class
+#' @export
+checkmate::assert_class
+
+
+
+#' @importFrom checkmate expect_class
+#' @export
+checkmate::expect_class
