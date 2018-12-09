32a23d496a91e36aed319e6814ea6c2661aa176e
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 15:08:51 -0500

    Export `hasDims` family of functions

---
+++ R/hasDims.R
@@ -0,0 +1,48 @@
+# TODO Needs examples.
+
+#' Does the Input Have Dimensions?
+#' @name hasDims
+#' @inherit params
+NULL
+
+
+
+#' @rdname hasDims
+#' @importFrom assertive.properties has_dims
+#' @export
+hasDims <- assertive.properties::has_dims
+
+
+
+#' @rdname hasDims
+#' @importFrom assertive.properties has_dimnames
+#' @export
+hasDimnames <- assertive.properties::has_dimnames
+
+
+
+#' @rdname hasDims
+#' @importFrom assertive.properties has_rows
+#' @export
+hasRows <- assertive.properties::has_rows
+
+
+
+#' @rdname hasDims
+#' @importFrom assertive.properties has_rownames
+#' @export
+hasRownames <- assertive.properties::has_rownames
+
+
+
+#' @rdname hasDims
+#' @importFrom assertive.properties has_cols
+#' @export
+hasCols <- assertive.properties::has_cols
+
+
+
+#' @rdname hasDims
+#' @importFrom assertive.properties has_colnames
+#' @export
+hasColnames <- assertive.properties::has_colnames
