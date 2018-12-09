347350fab5ab000247be5d1ed9aff65f2325cfa2
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 06:56:41 -0500

    Reexport `checkAtomic()`

---
+++ R/checkAtomic.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkAtomic
+#' @export
+checkmate::checkAtomic
+
+#' @importFrom checkmate check_atomic
+#' @export
+checkmate::check_atomic
+
+
+
+#' @importFrom checkmate testAtomic
+#' @export
+checkmate::testAtomic
+
+#' @importFrom checkmate test_atomic
+#' @export
+checkmate::test_atomic
+
+
+
+#' @importFrom checkmate assertAtomic
+#' @export
+checkmate::assertAtomic
+
+#' @importFrom checkmate assert_atomic
+#' @export
+checkmate::assert_atomic
+
+
+
+#' @importFrom checkmate expect_atomic
+#' @export
+checkmate::expect_atomic
