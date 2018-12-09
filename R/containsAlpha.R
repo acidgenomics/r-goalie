0e6a797b3f637563c340dccd45aac9bf9da71381
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 18:38:57 -0500

    `containsAlpha()`: Rename and simplify

---
+++ R/containsAlpha.R
@@ -0,0 +1,36 @@
+#' Does the Argument Contain an Alpha Level?
+#'
+#' An alpha level must be a `scalar numeric` greater than 0 and less than 1.
+#'
+#' @name containsAlpha
+#' @inherit params
+#'
+#' @examples
+#' ## Pass ====
+#' containsAlpha(0.05)
+#'
+#' ## Fail ====
+#' containsAlpha("xxx")
+#' containsAlpha(1L)
+NULL
+
+
+
+.containsAlpha <- function(x) {
+    msg <- "An alpha level must be a scalar numeric > 0 and < 1"
+    ok <- is_scalar_double(x)
+    if (!isTRUE(ok)) {
+        return(msg)
+    }
+    ok <- x > 0L && x < 1L
+    if (!isTRUE(ok)) {
+        return(msg)
+    }
+    TRUE
+}
+
+
+
+#' @rdname containsAlpha
+#' @export
+containsAlpha <- makeTestFunction(.containsAlpha)
