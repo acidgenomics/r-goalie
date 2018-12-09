ebd3bb09123174ed09e5151ba702da7bc759ac78
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 08:12:41 -0500

    Reexport `checkTibble()`

---
+++ R/checkmate-checkTibble.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkTibble
+#' @export
+checkmate::checkTibble
+
+#' @importFrom checkmate check_tibble
+#' @export
+checkmate::check_tibble
+
+
+
+#' @importFrom checkmate testTibble
+#' @export
+checkmate::testTibble
+
+#' @importFrom checkmate test_tibble
+#' @export
+checkmate::test_tibble
+
+
+
+#' @importFrom checkmate assertTibble
+#' @export
+checkmate::assertTibble
+
+#' @importFrom checkmate assert_tibble
+#' @export
+checkmate::assert_tibble
+
+
+
+#' @importFrom checkmate expect_tibble
+#' @export
+checkmate::expect_tibble
