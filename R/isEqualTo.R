efab4c55d2855f27b3a5b2e71dd023aacfd2ea9b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 13:28:27 -0500

    Rework verb approach

---
+++ R/isEqualTo.R
@@ -0,0 +1,46 @@
+#' How Does the Input Relate to a Value?
+#' @name isEqualTo
+#' @inherit params
+NULL
+
+
+
+#' @rdname isEqualTo
+#' @inheritParams assertive.numbers::is_equal_to
+#' @export
+isEqualTo <- assertive.numbers::is_equal_to
+
+
+
+#' @rdname isEqualTo
+#' @inheritParams assertive.numbers::is_not_equal_to
+#' @export
+isNotEqualTo <- assertive.numbers::is_not_equal_to
+
+
+
+#' @rdname isEqualTo
+#' @inheritParams assertive.numbers::is_greater_than
+#' @export
+isGreaterThan <- assertive.numbers::is_greater_than
+
+
+
+#' @rdname isEqualTo
+#' @inheritParams assertive.numbers::is_greater_than_or_equal_to
+#' @export
+isGreaterThanOrEqualTo <- assertive.numbers::is_greater_than_or_equal_to
+
+
+
+#' @rdname isEqualTo
+#' @inheritParams assertive.numbers::is_less_than
+#' @export
+isLessThan <- assertive.numbers::is_less_than
+
+
+
+#' @rdname isEqualTo
+#' @inheritParams assertive.numbers::is_less_than_or_equal_to
+#' @export
+isLessThanOrEqualTo <- is_less_than_or_equal_to
---
+++ R/isInRange.R
@@ -0,0 +1,79 @@
+#' Is the Input in Range?
+#' @name inRange
+#' @inherit params
+NULL
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_in_range
+#' @export
+isInRange <- assertive.numbers::is_in_range
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_in_closed_range
+#' @export
+isInClosedRange <- assertive.numbers::is_in_closed_range
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_in_open_range
+#' @export
+isInOpenRange <- assertive.numbers::is_in_open_range
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_in_left_open_range
+#' @export
+isInLeftOpenRange <- assertive.numbers::is_in_left_open_range
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_in_right_open_range
+#' @export
+isInRightOpenRange <- assertive.numbers::is_in_right_open_range
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_negative
+#' @export
+isNegative <- assertive.numbers::is_negative
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_non_negative
+#' @export
+isNonNegative <- assertive.numbers::is_non_negative
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_positive
+#' @export
+isPositive <- assertive.numbers::is_positive
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_non_positive
+#' @export
+isNonPositive <- assertive.numbers::is_non_positive
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_percentage
+#' @export
+isPercentage <- assertive.numbers::is_percentage
+
+
+
+#' @rdname inRange
+#' @importFrom assertive.numbers is_proportion
+#' @export
+isProportion <- assertive.numbers::is_proportion
