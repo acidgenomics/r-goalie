e42b3edc6025d8de3956ae9c223ff347aeff1c66
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-09 09:59:26 -0500

    Reexport `checkMatrix()`

---
+++ R/checkmate-checkMatrix.R
@@ -0,0 +1,33 @@
+#' @importFrom checkmate checkMatrix
+#' @export
+checkmate::checkMatrix
+
+#' @importFrom checkmate check_matrix
+#' @export
+checkmate::check_matrix
+
+
+
+#' @importFrom checkmate testMatrix
+#' @export
+checkmate::testMatrix
+
+#' @importFrom checkmate test_matrix
+#' @export
+checkmate::test_matrix
+
+
+
+#' @importFrom checkmate assertMatrix
+#' @export
+checkmate::assertMatrix
+
+#' @importFrom checkmate assert_matrix
+#' @export
+checkmate::assert_matrix
+
+
+
+#' @importFrom checkmate expect_matrix
+#' @export
+checkmate::expect_matrix
