c73c215f941f68081841492d71bb175a1bcc791b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-08 20:35:47 -0500

    Initial batch of checkmate reexports

---
+++ R/reexports-checkmate.R
@@ -0,0 +1,117 @@
+# checkClass ===================================================================
+#' @importFrom checkmate assertClass
+#' @export
+checkmate::assertClass
+
+#' @importFrom checkmate checkClass
+#' @export
+checkmate::checkClass
+
+#' @importFrom checkmate testClass
+#' @export
+checkmate::testClass
+
+
+
+# checkDirectoryExists =========================================================
+#' @importFrom checkmate assertDirectoryExists
+#' @export
+checkmate::assertDirectoryExists
+
+#' @importFrom checkmate checkDirectoryExists
+#' @export
+checkmate::checkDirectoryExists
+
+#' @importFrom checkmate testDirectoryExists
+#' @export
+checkmate::testDirectoryExists
+
+
+
+# checkFileExists ==============================================================
+#' @importFrom checkmate assertFileExists
+#' @export
+checkmate::assertFileExists
+
+#' @importFrom checkmate checkFileExists
+#' @export
+checkmate::checkFileExists
+
+#' @importFrom checkmate testFileExists
+#' @export
+checkmate::testFileExists
+
+
+
+# checkFlag ====================================================================
+#' @importFrom checkmate assertFlag
+#' @export
+checkmate::assertFlag
+
+#' @importFrom checkmate checkFlag
+#' @export
+checkmate::checkFlag
+
+#' @importFrom checkmate testFlag
+#' @export
+checkmate::testFlag
+
+
+
+# checkLogical =================================================================
+#' @importFrom checkmate assertLogical
+#' @export
+checkmate::assertLogical
+
+#' @importFrom checkmate checkLogical
+#' @export
+checkmate::checkLogical
+
+#' @importFrom checkmate testLogical
+#' @export
+checkmate::testLogical
+
+
+
+# checkNames ===================================================================
+#' @importFrom checkmate assertNames
+#' @export
+checkmate::assertNames
+
+#' @importFrom checkmate checkNames
+#' @export
+checkmate::checkNames
+
+#' @importFrom checkmate testNames
+#' @export
+checkmate::testNames
+
+
+
+# checkScalar ==================================================================
+#' @importFrom checkmate assertScalar
+#' @export
+checkmate::assertScalar
+
+#' @importFrom checkmate checkScalar
+#' @export
+checkmate::checkScalar
+
+#' @importFrom checkmate testScalar
+#' @export
+checkmate::testScalar
+
+
+
+# checkString ==================================================================
+#' @importFrom checkmate assertString
+#' @export
+checkmate::assertString
+
+#' @importFrom checkmate checkString
+#' @export
+checkmate::checkString
+
+#' @importFrom checkmate testString
+#' @export
+checkmate::testString
