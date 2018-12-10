7b5f9f89125032282501a4dbdcb63d3b584cdf84
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-10 09:52:33 -0500

    Add `isDir()` and `isFile()` checks

--- R/isFile.R
+++ R/isFile.R
@@ -1,4 +1,40 @@
-# FIXME Add this.
+#' Does the Argument Contain a Single (Existing) File?
+#'
+#' @name isFile
+#'
+#' @param x `character(1)`. String specifying a local file path that must exist
+#'   and be accessible.
+#'
+#' @examples
+#' x <- "example.txt"
+#' file.create(x)
+#' isFile(x)
+#' unlink(x)
+NULL
 
-# assertString(file)
-# assertFileExists(file)
+
+
+.isFile <- function(x) {
+    ok <- isString(x)
+    if (!isTRUE(ok)) {
+        return("Must contain string")
+    }
+
+    ok <- dir.exists(x)
+    if (!isTRUE(ok)) {
+        return("File does not exist")
+    }
+
+    ok <- hasAccess(x)
+    if (!isTRUE(ok)) {
+        return("File does not have read access")
+    }
+
+    TRUE
+}
+
+
+
+#' @rdname isFile
+#' @export
+isFile <- makeTestFunction(.isFile)
