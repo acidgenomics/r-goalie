b77bfa5f87e8f612bec67bb501f1e21a9878e51b
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-10 10:05:01 -0500

    Reexport `isDirectory()` and `isFile()` checks

---
+++ R/isDirectory.R
@@ -0,0 +1,37 @@
+#' Does the Argument Contain a Directory?
+#'
+#' @name isDirectory
+#' @importFrom R.utils isDirectory
+#' @inherit params
+#'
+#' @examples
+#' isADirectory("~")
+NULL
+
+
+
+#' @rdname isDirectory
+#' @export
+isDirectory <- function(x) {
+    R.utils::isDirectory(x)
+}
+
+
+
+#' @rdname isDirectory
+#' @export
+isDir <- isDirectory
+
+
+
+#' @rdname isDirectory
+#' @export
+isADirectory <- function(x) {
+    isString(x) && isDirectory(x)
+}
+
+
+
+#' @rdname isDirectory
+#' @export
+isADir <- isADirectory
