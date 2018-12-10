8f244bdf02b19d814bcc108479a36c687f2c8a54
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-10 12:23:10 -0500

    Finish updating documentation

--- R/range.R
+++ R/range.R
@@ -1,6 +1,6 @@
 #' Is the Input in Range?
 #'
-#' @name inRange
+#' @name range
 #' @inherit params
 #' @inheritParams assertive.numbers::is_in_range
 #'
@@ -27,76 +27,76 @@
 NULL
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_in_range
 #' @export
 isInRange <- is_in_range
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_in_closed_range
 #' @export
 isInClosedRange <- is_in_closed_range
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_in_open_range
 #' @export
 isInOpenRange <- is_in_open_range
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_in_left_open_range
 #' @export
 isInLeftOpenRange <- is_in_left_open_range
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_in_right_open_range
 #' @export
 isInRightOpenRange <- is_in_right_open_range
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_negative
 #' @export
 isNegative <- is_negative
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_non_negative
 #' @export
 isNonNegative <- is_non_negative
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_positive
 #' @export
 isPositive <- is_positive
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_non_positive
 #' @export
 isNonPositive <- is_non_positive
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_percentage
 #' @export
 isPercentage <- is_percentage
 
 
 
-#' @rdname inRange
+#' @rdname range
 #' @importFrom assertive.numbers is_proportion
 #' @export
 isProportion <- is_proportion
