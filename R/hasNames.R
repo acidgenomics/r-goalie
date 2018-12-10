a6a4390d3213cc5e10a03bd0a1b7ef42877ec31f
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-10 06:06:02 -0500

    Re-add `hasNames()` and `hasUniqueCols()`

---
+++ R/hasNames.R
@@ -0,0 +1,10 @@
+#' Does the Object Have Names?
+#'
+#' @importFrom assertive.properties has_names
+#' @inherit params
+#' @export
+#'
+#' @examples
+#' x <- datasets::mtcars
+#' hasNames(x)
+hasNames <- has_names
