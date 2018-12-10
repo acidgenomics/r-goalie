fcde57ef483a424eb8e923dbb9ef0626c3b5d4cc
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-10 10:48:42 -0500

    Tighten up `hasLength()` check

--- R/hasLength.R
+++ R/hasLength.R
@@ -1,1 +1,42 @@
-# FIXME placeholder.
+#' Does the Input Have A Non-Zero Length?
+#'
+#' @name hasLength
+#' @inherit params
+#'
+#' @param n `NULL` or `integer`. If `NULL` (default), the function will
+#'   check to see if the input length is non-zero.
+#'
+#' @examples
+#' ## Pass ====
+#' hasLength(1L)
+#' hasLength(FALSE)
+#' hasLength(datasets::mtcars)
+#' hasLength("")
+#'
+#' hasLength(
+#'
+#' ## Fail ====
+#' hasLength(NULL)
+#' hasLength(character())
+#' hasLength(data.frame())
+NULL
+
+
+
+.hasLength <- function(x, n = NULL) {
+    length <- length(x)
+    if (length == 0L) {
+        return(FALSE)
+    }
+    if (is.null(n)) {
+        return(length > 0L)
+    }
+    assert(isInt(n), n > 0L)
+    length == n
+}
+
+
+
+#' @rdname hasLength
+#' @export
+hasLength <- makeTestFunction(.hasLength)
