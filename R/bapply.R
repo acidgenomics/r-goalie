89830f00558b37d4c4444d98d490e43a44c23b9f
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-15 06:50:08 -0500

    Export `bapply()`, based on `assertive.base::bapply()`

---
+++ R/bapply.R
@@ -0,0 +1,29 @@
+#' Apply a Function over a List or Vector
+#'
+#' `bapply` is a wrapper to `vapply` for functions that return `logical(1)`.
+#'
+#' @note `USE.NAMES` is always set to TRUE.
+#'
+#' @param x `atomic` or `list`.
+#' @param predicate `function`. A predicate function that returns `logical(1)`
+#'   (boolean flag) to `apply`.
+#' @param ... Additional arguments passed to [vapply].
+#'
+#' @seealso
+#' - `vapply`.
+#' - `assertive.base::bapply`.
+#'
+#' @return `logical`.
+#' @export
+#'
+#' @examples
+#' bapply(list(a = "example", b = 1), is.character)
+bapply <- function(x, predicate, ...) {
+    vapply(
+        X = x,
+        FUN = predicate,
+        FUN.VALUE = logical(1L),
+        ...,
+        USE.NAMES = TRUE
+    )
+}
