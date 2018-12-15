a3886e771a07bfae52c5167fa697f6b178686ac3
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-15 08:52:43 -0500

    Export `safeDeparse()`

---
+++ R/base-safeDeparse.R
@@ -0,0 +1,17 @@
+#' Safe version of deparse
+#'
+#' A version of `deparse` that is guaranteed to always return `character(1)`.
+#'
+#' @export
+#'
+#' @param expr `expression`.
+#'   Any R expression.
+#' @param ... Passed to `deparse`.
+#'
+#' @seealso `assertive.base::safe_deparse`.
+#'
+#' @examples
+#' safeDeparse(is.character("a"))
+safeDeparse <- function(expr, ...) {
+    paste0(deparse(expr, width.cutoff = 500L, ...), collapse = "")
+}
