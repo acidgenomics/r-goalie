8f55bde9e832baf6cf58b7b7e188c8342d14836b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-17 16:31:24 -0500

    Use assertive methods internally

--- R/check-hasDuplicates.R
+++ R/check-hasDuplicates.R
@@ -4,6 +4,10 @@
 #' @importFrom assertive.properties has_duplicates has_no_duplicates
 #' @inherit params
 #'
+#' @seealso
+#' - `assertive.properties::has_duplicates()`.
+#' - `assertive.properties::has_no_duplicates()`.
+#'
 #' @examples
 #' hasDuplicates(c("a", "a"))
 #' hasNoDuplicates(c("a", "b"))
@@ -13,10 +17,29 @@ NULL
 
 #' @rdname hasDuplicates
 #' @export
-hasDuplicates <- has_duplicates
+hasDuplicates <- function (x, .xname = getNameInParent(x)) {
+    if (!anyDuplicated(x)) {
+        return(false(gettext("%s has no duplicates."), .xname))
+    }
+    TRUE
+}
 
 
 
 #' @rdname hasDuplicates
 #' @export
-hasNoDuplicates <- has_no_duplicates
+hasNoDuplicates <- function (x, .xname = getNameInParent(x)) {
+    if (anyDuplicated(x)) {
+        dupeIndicies <- which(duplicated(x))
+        return(false(
+            ngettext(
+                length(dupeIndicies),
+                "%s has a duplicate at position %s.",
+                "%s has duplicates at positions %s."
+            ),
+            .xname,
+            toString(dupeIndicies, width = 100L)
+        ))
+    }
+    TRUE
+}
