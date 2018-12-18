14179ff260966fc080a315f4b4c1f2fda9a90271
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 16:02:19 -0500

    Tighten up assert checks

--- R/check-hasLength.R
+++ R/check-hasLength.R
@@ -18,18 +18,18 @@
 #' hasLength(NULL)
 #' hasLength(character())
 #' hasLength(data.frame())
-hasLength <- function(x, n = NULL) {
-    xname <- getNameInParent(x)
+hasLength <- function(x, n = NULL, .xname = getNameInParent(x)) {
     length <- length(x)
 
     if (length == 0L) {
-        return(false("%s has length 0.", xname))
+        return(false("%s has length 0.", .xname))
     }
 
     if (is.null(n)) {
-        return(length > 0L)
+        return(TRUE)
+    } else {
+        assert(isInt(n), n > 0L)
     }
-    assert(isInt(n), n > 0L)
 
     ok <- length == n
     if (!isTRUE(ok)) {
