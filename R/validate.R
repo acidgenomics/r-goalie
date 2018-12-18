4341e2796f5b910b9277ab5dc991bbfa2f0eeaca
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 10:58:39 -0500

    Add comments about improving `validate()` engine

--- R/validate.R
+++ R/validate.R
@@ -1,27 +1,28 @@
 # FIXME Can we get rid of the assertthat dependency here?
+# Simplify using assertive engine approach here.
+
+
 
 #' Validate an S4 Class
 #'
-#' `validate` is a variant of `assert` that is specifically intended to be
+#' [validate()] is a variant of [assert()] that is specifically intended to be
 #' used inside of an S4 validity method definition.
 #'
-#' Currently, `validate` reexports `assertthat::validate_that`.
-#'
-#' Like `assert`, `validate` returns `TRUE` on success. However, on failure
-#' it returns a `character` instead of a `stop` call. This is the current
-#' recommended practice for defining S4 validity methods inside of a
-#' `setValidity` call. Refer to the documentation in the methods package,
-#' specifically on `validObject` for detailed information on S4 validity
-#' methods.
+#' Like [assert()], [validate()] returns `TRUE` on success. However, on failure
+#' it returns a `character` instead of a [stop()][base::stop] call. This is the
+#' current recommended practice for defining S4 validity methods inside of a
+#' [setValidity()][methods::setValidity] call. Refer to the documentation in the
+#' methods package, specifically [validObject()][methods::validObject] for
+#' detailed information on S4 validity methods.
 #'
 #' @importFrom assertthat validate_that
 #' @inheritParams assertthat::validate_that
 #' @export
 #'
 #' @seealso
-#' - `methods::setValidity`.
-#' - `methods::validObject`.
-#' - `assertthat::validate_that`.
+#' - `methods::setValidity()`.
+#' - `methods::validObject()`.
+#' - `assertthat::validate_that()`.
 #'
 #' @examples
 #' validate(
