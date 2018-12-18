003cacc8939449df2c4420a91801c3af163fb1b7
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 17:55:17 -0500

    Allow conditional NULL passthrough, which is super useful for formals

--- R/check-isString.R
+++ R/check-isString.R
@@ -11,20 +11,27 @@
 #' isString(1)
 #' isString("")
 #' isString(NA_character_)
-isString <- function(x) {
-    xname <- getNameInParent(x)
+isString <- function(
+    x,
+    nullOK = FALSE,
+    .xname = getNameInParent(x)
+) {
+    # Conditionally allow NULL.
+    if (isTRUE(nullOK) && is.null(x)) {
+        return(TRUE)
+    }
 
     ok <- is.character(x) && length(x) == 1L
     if (!isTRUE(ok)) {
-        return(false("%s is not a character of length 1.", xname))
+        return(false("%s is not a character of length 1.", .xname))
     }
 
     # Return FALSE on NA character or empty string.
     if (is.na(x)) {
-        return(false("%s is NA.", xname))
+        return(false("%s is NA.", .xname))
     }
     if (identical(x, "")) {
-        return(false("%s contains empty string.", xname))
+        return(false("%s contains empty string.", .xname))
     }
 
     TRUE
