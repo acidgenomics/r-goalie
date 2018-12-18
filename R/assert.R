7e029553a5436a9bd4251e72ca0b533e7a476a56
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 10:19:13 -0500

    Placeholder updates for `assert()`

--- R/assert.R
+++ R/assert.R
@@ -16,6 +16,7 @@
 #'
 #' @seealso
 #' - `stopifnot()`.
+#' - `assertive.base::assert_engine()`.
 #' - `assertthat::assert_that()`.
 #' - `checkmate::assert()`.
 #'
@@ -28,7 +29,7 @@ assert <- stopifnot
 
 
 
-# Draft update simplifying `stopifnot`, but including better error messages.
+# `base::stopifnot()` modification.
 # assert <- function(...) {
 #     cl <- match.call()[-1L]
 #     Dparse <- function(call, cutoff = 60L) {
@@ -91,4 +92,105 @@ assert <- stopifnot
 #         }
 #     }
 #     invisible()
+# }
+
+
+
+# `assertthat::assert_that()` modification.
+# assert <- function (..., env = parent.frame(), msg = NULL) {
+#     res <- see_if(..., env = env, msg = msg)
+#     if (res)
+#         return(TRUE)
+#     stop(assertError(attr(res, "msg")))
+# }
+
+
+
+# `assertive::assert_engine()` modification.
+# assert <- function(
+#     predicate,
+#     ...,
+#     msg = "The assertion failed.",
+#     what = c("all", "any"),
+#     na_ignore = FALSE,
+#     severity = c("stop", "warning", "message", "none")
+# ) {
+#     handler_type <- match.arg(severity)
+#     dots <- list(...)
+#     return_value <- if (length(dots) > 0)
+#         dots[[1]]
+#     else NULL
+#     if (handler_type == "none") {
+#         return(invisible(return_value))
+#     }
+#     what <- match.fun(match.arg(what))
+#     predicate_name <- get_name_in_parent(predicate)
+#     ok <- predicate(...)
+#     if (inherits(ok, "scalar_with_cause")) {
+#         if (!isTRUE(ok)) {
+#             if (missing(msg)) {
+#                 msg <- cause(ok)
+#             }
+#             give_feedback(handler_type, msg, predicate_name)
+#         }
+#     }
+#     else {
+#         really_ok <- if (na_ignore) {
+#             ok | is.na(ok)
+#         }
+#         else {
+#             ok & !is.na(ok)
+#         }
+#         if (!what(really_ok)) {
+#             msg <- paste(enc2utf8(msg), print_and_capture(ok),
+#                          sep = "\n")
+#             give_feedback(handler_type, msg, predicate_name)
+#         }
+#     }
+#     invisible(return_value)
+# }
+
+
+
+
+# `checkmate::assert()` modification.
+# assert <- function(..., combine = "or", .var.name = NULL) {
+#     assertChoice(combine, c("or", "and"))
+#     dots = match.call(expand.dots = FALSE)$...
+#     env = parent.frame()
+#     if (combine == "or") {
+#         msgs = character(length(dots))
+#         for (i in seq_along(dots)) {
+#             val = eval(dots[[i]], envir = env)
+#             if (identical(val, TRUE))
+#                 return(invisible(TRUE))
+#             msgs[i] = as.character(val)
+#         }
+#         if (is.null(.var.name))
+#             .var.name = vapply(dots, function(x) as.character(x)[2L],
+#                                FUN.VALUE = NA_character_)
+#         if (length(msgs) > 1L) {
+#             msgs = sprintf("%s(%s): %s", vapply(dots, function(x) as.character(x)[1L],
+#                                                 FUN.VALUE = NA_character_), .var.name, msgs)
+#             msgs = paste0(c("One of the following must apply:",
+#                             strwrap(msgs, prefix = " * ")), collapse = "\n")
+#             mstop("Assertion failed. %s", msgs)
+#         }
+#         else {
+#             mstop("Assertion on '%s' failed. %s.", .var.name,
+#                   msgs)
+#         }
+#     }
+#     else {
+#         for (i in seq_along(dots)) {
+#             val = eval(dots[[i]], envir = env)
+#             if (!identical(val, TRUE)) {
+#                 if (is.null(.var.name))
+#                     .var.name = as.character(dots[[1L]])[2L]
+#                 mstop("Assertion on '%s' failed. %s.", .var.name,
+#                       val)
+#             }
+#         }
+#     }
+#     invisible(TRUE)
 # }
