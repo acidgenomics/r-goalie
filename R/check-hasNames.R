9e3ba73f373af0905f7ffaf086573bdc144b0ce5
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 16:28:22 -0500

    Improve return causes

--- R/check-hasNames.R
+++ R/check-hasNames.R
@@ -6,16 +6,19 @@
 #' @seealso `assertive.properties::has_names()`.
 #'
 #' @examples
-#' x <- datasets::mtcars
-#' hasNames(x)
-hasNames <- function(x) {
-    xname = getNameInParent(x)
+#' ## Pass ====
+#' hasNames(datasets::mtcars)
+#'
+#' ## Fail ====
+#' hasNames(matrix())
+#' hasNames(data.frame())
+hasNames <- function(x, .xname = getNameInParent(x)) {
     namesx <- names(x)
     if (is.null(namesx)) {
-        return(false("The names of %s are NULL.", xname))
+        return(false("The names of %s are NULL.", .xname))
     }
     if (!any(nzchar(namesx))) {
-        return(false("The names of %s are all empty.", xname))
+        return(false("The names of %s are all empty.", .xname))
     }
     TRUE
 }
