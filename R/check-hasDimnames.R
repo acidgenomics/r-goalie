d1f34ef172ab265a7520441caa801708fe1a8eee
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 15:13:18 -0500

    Improve `has*()` checks

---
+++ R/check-hasDimnames.R
@@ -0,0 +1,54 @@
+#' Does the input have dimnames?
+#'
+#' @export
+#' @inherit params
+#'
+#' @seealso
+#' - `hasRownames()`.
+#' - `assertive.properties::has_dimnames()`.
+#' - `assertive.properties::has_colnames()`.
+#'
+#' @examples
+#' ## Pass ====
+#' x <- datasets::mtcars
+#' hasDimnames(x)
+#' hasRownames(x)
+#' hasColnames(x)
+#'
+#' ## Fail ====
+#' x <- data.frame()
+#' hasDimnames(x)
+#' hasRownames(x)
+#' hasColnames(x)
+hasDimnames <- function(x) {
+    xname = getNameInParent(x)
+    dimnamesx <- dimnames(x)
+    if (is.null(dimnamesx)) {
+        return(false("The dimension names of %s are NULL.", xname))
+    }
+    if (!any(nzchar(unlist(dimnamesx, use.names = FALSE)))) {
+        return(false("The dimension names of %s are all empty.", xname))
+    }
+    TRUE
+}
+
+
+
+# Documenting `hasRownames()` in a separate Rd file because it's complicated.
+
+
+
+#' @rdname hasDims
+#' @importFrom assertive.properties has_colnames
+#' @export
+hasColnames <- function(x) {
+    xname = getNameInParent(x)
+    colnamesx <- colnames(x)
+    if (is.null(colnamesx)) {
+        return(false("The column names of %s are NULL.", xname))
+    }
+    if (!any(nzchar(colnamesx))) {
+        return(false("The column names of %s are all empty.", xname))
+    }
+    TRUE
+}
