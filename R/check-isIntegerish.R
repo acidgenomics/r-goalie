505f40210ddace218c8507fd6f986a0361c93546
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 17:49:28 -0500

    Call rlang internally, which uses Rcpp

--- R/check-isIntegerish.R
+++ R/check-isIntegerish.R
@@ -1,20 +1,19 @@
 #' Is the Input Integerish?
 #'
 #' Check for valid input of either explicit (e.g. `1L`) and/or implict
-#' (e.g. `1`) integer.
+#' (e.g. `1`) `integer`.
 #'
-#' @name isIntegerish
-#' @importFrom rlang is_integerish
-#' @inherit params
-#' @inheritParams rlang::is_integerish
 #' @export
+#' @inherit params
 #'
 #' @seealso
-#' - `rlang::is_integerish`.
-#' - `checkmate::checkIntegerish`.
-#' - `assertive::is_a_number`.
+#' - `rlang::is_integerish()`.
+#' - `checkmate::checkIntegerish()`.
 #'
 #' @examples
 #' isIntegerish(seq_len(2L))
 #' isIntegerish(c(1, 2))
-isIntegerish <- is_integerish
+isIntegerish <- function(x) {
+    requireNamespace("rlang", quietly = TRUE)
+    rlang::is_integerish(x)
+}
