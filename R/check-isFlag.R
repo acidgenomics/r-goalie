f0d8a3687233f1543cd1e1dcf7558c126c920b9f
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-18 17:48:25 -0500

    Improve the boolean flag return cause message

--- R/check-isFlag.R
+++ R/check-isFlag.R
@@ -12,17 +12,15 @@
 #' isFlag(c(TRUE, TRUE))
 #' isFlag(1)
 #' isFlag(NA)
-isFlag <- function(x) {
-    xname <- getNameInParent(x)
-
-    ok <- is.logical(x) && length(x) == 1L
+isFlag <- function(x, .xname = getNameInParent(x)) {
+    ok <- isScalarLogical(x)
     if (!isTRUE(ok)) {
-        return(false("%s is not a boolean flag (TRUE/FALSE).", xname))
+        return(false("%s is not a boolean flag (TRUE/FALSE).", .xname))
     }
 
     # Check for NA, which is logical but not a flag.
     if (is.na(x)) {
-        return(false("%s is NA", xname))
+        return(false("%s is NA", .xname))
     }
 
     TRUE
