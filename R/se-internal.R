abd4b91850e905097547514bc700b46df3d24e95
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-19 16:46:54 -0500

    Reorganize this file

--- R/se-internal.R
+++
@@ -1,5 +0,0 @@
-# Using primary assay here.
-.coerceSummarizedExperimentToMatrix <- function(object) {
-    requireNamespace("SummarizedExperiment", quietly = TRUE)
-    SummarizedExperiment::assay(object)
-}
