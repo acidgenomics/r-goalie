a507db4e00c003b07f16dbae926a2cdd268b9f9b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-19 16:45:15 -0500

    Reorganize engine functions

--- R/base-assertive-DIM.R
+++
@@ -1,9 +0,0 @@
-# `assertive.properties::DIM()`.
-DIM <- function(x) {
-    dim_x <- dim(x)
-    if (is.null(dim_x)) {
-        length(x)
-    } else {
-        dim_x
-    }
-}
--- R/base-stopifnot-Dparse.R
+++
@@ -1,9 +0,0 @@
-# @seealso `base::stopifnot()`.
-Dparse <- function(call, cutoff = 60L) {
-    ch <- deparse(call, width.cutoff = cutoff)
-    if (length(ch) > 1L) {
-        paste(ch[1L], "....")
-    } else {
-        ch
-    }
-}
