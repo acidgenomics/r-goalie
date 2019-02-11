52a66eb29df39fcd5f13cdb41e775153c127d92e
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-11 11:13:42 -0500

    Attempt to add support for `...elt()` to maintain compatibility with R 3.4

---
+++ R/onLoad.R
@@ -0,0 +1,13 @@
+# Import backports necessary to maintain R 3.4 support.
+
+# `...elt()` is used inside of `assert()`.
+# help(topic = "...elt", package = "backports")
+
+.major <- as.numeric(version[["major"]])
+.minor <- as.numeric(version[["minor"]])
+
+if (isTRUE(.major >= 3L) && isTRUE(.minor < 5L)) {
+    .onLoad <- function(libname, pkgname) {
+        backports::import(pkgname = pkgname, obj = "...elt")
+    }
+}
