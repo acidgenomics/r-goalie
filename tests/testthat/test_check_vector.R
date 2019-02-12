4b8a263c10c1656a5e236217289a52e585d6e3ff
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-11 20:49:25 -0500

    Improve code coverage for vectorized functions

--- tests/testthat/test_check_vector.R
+++ tests/testthat/test_check_vector.R
@@ -21,10 +21,73 @@ test_that("hasAccess", {
     )
 })
 
-# isDirectory
-# isEqual
-# isExisting
-# isFile
+test_that("isDirectory", {
+    expect_identical(
+        isDirectory(c("~", ".")),
+        c(`~` = TRUE, `.` = TRUE)
+    )
+
+    # Note that this doesn't set cause attribute on failure here.
+    expect_identical(
+        isDirectory(c("aaa", "bbb")),
+        c(aaa = FALSE, bbb = FALSE)
+    )
+
+    expect_false(isDirectory(NULL))
+})
+
+test_that("isEqual", {
+    expect_true(isEqualTo(x = 1L, y = 1))
+    expect_true(isNotEqualTo(x = 2, y = 1))
+    expect_true(isGreaterThan(x = 1, y = 0))
+    expect_true(all(isGreaterThanOrEqualTo(x = seq_len(2), y = 1)))
+    expect_true(isLessThan(x = -1, y = 0))
+    expect_true(all(isLessThanOrEqualTo(x = seq_len(2), y = 3)))
+
+    object <- isEqualTo(x = seq_len(2), y = 1)
+    expect_identical(as.logical(object), c(TRUE, FALSE))
+    expect_identical(
+        cause(object),
+        noquote(c("", "not equal to 1; abs diff = 1"))
+    )
+})
+
+test_that("isExisting", {
+    a <- 1L
+    b <- 2L
+
+    expect_identical(
+        isExisting(c("a", "b")),
+        c(TRUE, TRUE)
+    )
+    expect_true(allAreNonExisting(c("x", "y")))
+
+    object <- isExisting(c("x", "y"))
+    expect_s3_class(object, "goalie")
+    expect_identical(as.logical(object), c(FALSE, FALSE))
+    expect_identical(
+        cause(object),
+        noquote(c("non-existing", "non-existing"))
+    )
+
+    object <- allAreExisting(c("x", "y"))
+    expect_s3_class(object, "goalie")
+    expect_false(object)
+})
+
+test_that("isFile", {
+    x <- "example.txt"
+    file.create(x)
+    expect_true(isAFile(x))
+    unlink(x)
+
+    expect_false(isFile(1L))
+    expect_identical(
+        isFile(c("~", ".")),
+        c(`~` = FALSE, `.` = FALSE)
+    )
+})
+
 # isHexColor
 # isInRange
 # isIntegerish
