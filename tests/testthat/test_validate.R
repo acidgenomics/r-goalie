961ea8f3e08fee246d9d69c1e041ef1026fd9379
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-12 08:08:34 -0500

    Add unit test for `validate()`

---
+++ tests/testthat/test_validate.R
@@ -0,0 +1,41 @@
+context("Validate")
+
+test_that("validate", {
+    expect_true(
+        validate(is.integer(1L))
+    )
+    expect_identical(
+        validate(is.character(1L)),
+        "is.character(1L) is not TRUE."
+    )
+
+})
+
+test_that("validateClasses", {
+    expect_true(
+        validateClasses(
+            object = list(
+                a = character(),
+                b = integer(),
+                c = factor()
+            ),
+            expected = list(
+                a = "character",
+                b = "integer",
+                c = "factor"
+            )
+        )
+    )
+
+    expect_identical(
+        validateClasses(
+            object = list(
+                a = character()
+            ),
+            expected = list(
+                a = "integer"
+            )
+        ),
+        "Class checks failed:\n[1] \"a\""
+    )
+})
