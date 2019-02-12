087efcff9db3b3e0dd07f9343c88149c1cdde09d
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-12 08:08:34 -0500

    Add unit test for `validate()`

---
+++ tests/testthat/test_engine.R
@@ -0,0 +1,17 @@
+context("Engine")
+
+# appendToBody
+# bapply
+# callAndName
+# capitalize
+# cause
+# false
+# getNameInParent
+# is2
+# matchArgsToDoCall
+# MethodDefinition
+# print
+# safeDeparse
+# setCause
+# standardizeCall
+
