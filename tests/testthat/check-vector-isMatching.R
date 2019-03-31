cb19296351dddcc7a9199bc2385eebeb638c6e0a
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 16:13:33 -0400

    Split out

---
+++ tests/testthat/check-vector-isMatching.R
@@ -0,0 +1,11 @@
+context("check : vector : isMatching")
+
+test_that("TRUE", {
+    expect_true(isMatchingRegex(x = "foobar", pattern = "^f"))
+    expect_true(isNotMatchingRegex(x = "foobar", pattern = "^b"))
+})
+
+test_that("FALSE", {
+    expect_true(isMatchingFixed(x = "foobar", pattern = "bar"))
+    expect_true(isNotMatchingFixed(x = "foo", pattern = "bar"))
+})
