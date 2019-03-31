310f06ea4feccc059face0b96a5cfad28f02b2be
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 14:59:49 -0400

    Break out unit tests

---
+++ tests/testthat/test-check-scalar-allAreAtomic.R
@@ -0,0 +1,26 @@
+context("check : scalar : allAreAtomic")
+
+test_that("TRUE", {
+    expect_true(allAreAtomic(data.frame(a = "foo", b = "bar")))
+    expect_true(allAreAtomic(list(a = "foo", b = "bar")))
+})
+
+test_that("FALSE : non-atomic", {
+    ok <- allAreAtomic(list(a = "x", b = list()))
+    expect_s3_class(ok, "goalie")
+    expect_false(ok)
+    expect_identical(
+        cause(ok),
+        noquote('Not all elements in list(a = "x", b = list()) are atomic.')
+    )
+})
+
+test_that("FALSE : length 0", {
+    ok <- allAreAtomic(data.frame())
+    expect_s3_class(ok, "goalie")
+    expect_false(ok)
+    expect_identical(
+        cause(ok),
+        noquote("data.frame() has length 0.")
+    )
+})
---
+++ tests/testthat/test-check-scalar-areSameLength.R
@@ -0,0 +1,20 @@
+context("check : scalar : areSameLength")
+
+test_that("TRUE", {
+    x <- list(a = 1L, b = 2L)
+    y <- list(c = 3L, d = 4L)
+    ok <- areSameLength(x = x, y = y)
+    expect_true(ok)
+})
+
+test_that("FALSE", {
+    x <- list(a = 1L)
+    y <- list(b = 2L, c = 3L)
+    ok <- areSameLength(x = x, y = y)
+    expect_s3_class(ok, "goalie")
+    expect_false(ok)
+    expect_identical(
+        cause(ok),
+        noquote("x does not have the same length as y.")
+    )
+})
