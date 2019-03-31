8c1d426fd90414561463412236a8d0dd4ee681a6
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 15:30:09 -0400

    Split out `validateClasses()` check

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -10,36 +10,3 @@ test_that("validate", {
     )
 
 })
-
-
-
-context("validateClasses")
-
-test_that("validateClasses", {
-    expect_true(
-        validateClasses(
-            object = list(
-                a = character(),
-                b = integer(),
-                c = factor()
-            ),
-            expected = list(
-                a = "character",
-                b = "integer",
-                c = "factor"
-            )
-        )
-    )
-
-    expect_identical(
-        validateClasses(
-            object = list(
-                a = character()
-            ),
-            expected = list(
-                a = "integer"
-            )
-        ),
-        "Class checks failed:\n[1] \"a\""
-    )
-})
