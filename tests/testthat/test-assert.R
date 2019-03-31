43f83b9b5451326639d7824ff2a236bac097f1ad
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 17:02:25 -0400

    Improve assert engine checks

--- tests/testthat/test-assert.R
+++ tests/testthat/test-assert.R
@@ -1,7 +1,42 @@
 context("assert")
 
-test_that("assert", {
-    expect_silent(assert(is.character("xxx")))
-    expect_error(assert(is.character(1L)))
-    expect_silent(assert(is.integer(1L), is.numeric(1L)))
+test_that("Success", {
+    expect_true(assert(is.character("xxx")))
+    expect_true(assert(is.integer(1L), is.numeric(1L)))
+})
+
+test_that("Expected failure", {
+    expect_error(
+        object = assert(is.character(1L)),
+        regexp = paste(
+            "Assert failure.",
+            "is.character\\(1L\\) is not TRUE.",
+            sep = "\n"
+        )
+    )
+})
+
+test_that("Not boolean", {
+    expect_error(
+        object = assert(c(TRUE, TRUE)),
+        regexp = "Check did not return a boolean flag \\(TRUE/FALSE\\)."
+    )
+    expect_error(
+        object = assert("xxx"),
+        regexp = "Check did not return a boolean flag \\(TRUE/FALSE\\)."
+    )
+})
+
+test_that("Invalid input, checking stop passthrough", {
+    expect_error(
+        object = assert(stop("XXX")),
+        regexp = "XXX"
+    )
+})
+
+test_that("Invalid input, checking warning passthrough", {
+    expect_error(
+        object = assert(warning("XXX")),
+        regexp = "Check did not return a boolean flag \\(TRUE/FALSE\\)."
+    )
 })
