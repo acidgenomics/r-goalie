b27cb2479019ebda5fa6cb379cf022d7c5044ec0
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 17:13:59 -0400

    Add checks

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -1,12 +1,21 @@
 context("validate")
 
-test_that("validate", {
+test_that("Success", {
     expect_true(
         validate(is.integer(1L))
     )
+})
+
+test_that("Failure", {
     expect_identical(
         validate(is.character(1L)),
         "is.character(1L) is not TRUE."
     )
+})
 
+test_that("Custom message", {
+    expect_identical(
+        validate(is.character(1L), msg = "custom message"),
+        "custom message"
+    )
 })
