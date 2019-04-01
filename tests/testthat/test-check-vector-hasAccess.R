878340f17d5e4450dc9fa95accd4d6b40a96024a
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 22:23:52 -0400

    Improve code coverage

--- tests/testthat/test-check-vector-hasAccess.R
+++ tests/testthat/test-check-vector-hasAccess.R
@@ -1,20 +1,48 @@
 context("hasAccess")
 
 test_that("TRUE", {
-    ok <- hasAccess(c("~", "."))
+    x <- c("~", ".")
+
+    ok <- hasAccess(x)
+    expect_true(all(ok))
     expect_identical(ok, c(`~` = TRUE, `.` = TRUE))
+
+    ok <- allHaveAccess(x)
+    expect_true(ok)
 })
 
 test_that("FALSE : no access", {
-    ok <- hasAccess(c("xxx", "yyy"))
+    x <- c("xxx", "yyy")
+
+    ok <- hasAccess(x)
     expect_s3_class(ok, "goalie")
     expect_identical(nocause(ok), c(xxx = FALSE, yyy = FALSE))
     expect_identical(cause(ok), noquote(c("no access", "no access")))
+
+    ok <- allHaveAccess(x)
+    expect_s3_class(ok, "goalie")
+    expect_false(ok)
 })
 
 test_that("FALSE : NULL input", {
     ok <- hasAccess(NULL)
     expect_s3_class(ok, "goalie")
     expect_false(ok)
-    expect_identical(cause(ok), noquote("x is not character."))
+    expect_identical(
+        cause(ok),
+        noquote("x is not character.")
+    )
+})
+
+test_that("FALSE : Invalid access codes", {
+    ok <- hasAccess(".", access = "xxx")
+    expect_s3_class(ok, "goalie")
+    expect_false(ok)
+    expect_identical(
+        cause(ok),
+        noquote(paste0(
+            "x doesn't contain valid access codes.", "\n",
+            "Combinations of 'r', 'w' and 'x' are allowed."
+        ))
+    )
 })
