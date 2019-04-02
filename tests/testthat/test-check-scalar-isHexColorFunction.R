16925f0af7a2f4059781d7ef15445745aa723c18
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-02 08:35:37 -0400

    Improve code coverage

--- tests/testthat/test-check-scalar-isHexColorFunction.R
+++ tests/testthat/test-check-scalar-isHexColorFunction.R
@@ -4,10 +4,17 @@ test_that("TRUE", {
     expect_true(isHexColorFunction(viridis::viridis))
 })
 
-test_that("FALSE", {
-    ok <- isHexColorFunction(ggplot2::scale_colour_manual)
-    expect_false(ok)
+test_that("FALSE : not a function", {
+    ok <- isHexColorFunction("viridis")
     expect_s3_class(ok, "goalie")
+    expect_false(ok)
+    expect_identical(cause(ok), noquote('"viridis" is not a function.'))
+})
+
+test_that("FALSE : no `n` formal", {
+    ok <- isHexColorFunction(ggplot2::scale_colour_manual)
+    expect_s3_class(ok, "goalie")
+    expect_false(ok)
     expect_identical(
         cause(ok),
         noquote("Hex color function must contain an `n` formal argument.")
