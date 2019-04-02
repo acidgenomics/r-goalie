2a3f64d5adf78a5dc99a33f6483867e13149bb74
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-02 08:38:59 -0400

    Improve code coverage

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -1,15 +1,28 @@
 context("validate")
 
 test_that("Success", {
-    expect_true(
-        validate(is.integer(1L))
-    )
+    expect_true(validate(is.integer(1L)))
 })
 
-test_that("Failure", {
+test_that("Expected failure", {
     expect_identical(
         validate(is.character(1L)),
         "is.character(1L) is not TRUE."
+    )
+})
+
+test_that("Character passthrough", {
+    expect_identical(validate("xxx"), "xxx")
+})
+
+test_that("Invalid input", {
+    expect_error(
+        validate(c(TRUE, FALSE)),
+        regexp = "logical\\(1\\)"
+    )
+    expect_error(
+        object = validate(c("aaa", "bbb")),
+        regexp = "character\\(1\\)"
     )
 })
 
