78e96dcdb098218316bb2efc21b9586c0453b09f
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-02 08:23:36 -0400

    Improve code coverage

--- tests/testthat/test-check-scalar-hasLength.R
+++ tests/testthat/test-check-scalar-hasLength.R
@@ -7,32 +7,31 @@ test_that("TRUE", {
     expect_true(hasLength(""))
 })
 
+test_that("FALSE : not expected length", {
+    ok <- hasLength(x = "xxx", n = 2L)
+    expect_s3_class(ok, "goalie")
+    expect_false(ok)
+    cause(ok)
+    expect_identical(cause(ok), noquote('"xxx" does not have a length of 2.'))
+})
+
 test_that("FALSE : NULL", {
     ok <- hasLength(NULL)
-    expect_false(ok)
     expect_s3_class(ok, "goalie")
-    expect_identical(
-        cause(ok),
-        noquote("NULL has length 0.")
-    )
+    expect_false(ok)
+    expect_identical(cause(ok), noquote("NULL has length 0."))
 })
 
 test_that("FALSE : empty character", {
     ok <- hasLength(character())
-    expect_false(ok)
     expect_s3_class(ok, "goalie")
-    expect_identical(
-        cause(ok),
-        noquote("character() has length 0.")
-    )
+    expect_false(ok)
+    expect_identical(cause(ok), noquote("character() has length 0."))
 })
 
 test_that("FALSE : empty data frame", {
     ok <- hasLength(data.frame())
-    expect_false(ok)
     expect_s3_class(ok, "goalie")
-    expect_identical(
-        cause(ok),
-        noquote("data.frame() has length 0.")
-    )
+    expect_false(ok)
+    expect_identical(cause(ok), noquote("data.frame() has length 0."))
 })
