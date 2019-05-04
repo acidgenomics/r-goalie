1422d6b55f0b4411d3cd6b7e8755e3ecf79fd7c3
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 14:34:30 -0400

    Attempt to pass checks on legacy containers

--- travis-docker.R
+++ travis-docker.R
@@ -1,4 +1,4 @@
 Sys.setenv(TZ = "America/New_York")
-rcmdcheck::rcmdcheck(args = "--no-manual")
+rcmdcheck::rcmdcheck(args = "--no-build-vignettes --no-manual --timings")
 BiocCheck::BiocCheck(`quit-with-status` = TRUE)
 lintr::lint_package()
