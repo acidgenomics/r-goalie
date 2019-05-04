8d5223a4cac40841682304b2c29afb5e6a6682a2
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 14:53:30 -0400

    Fix R CMD check args

--- travis-docker.R
+++ travis-docker.R
@@ -1,4 +1,15 @@
 Sys.setenv(TZ = "America/New_York")
-rcmdcheck::rcmdcheck(args = "--no-build-vignettes --no-manual --timings")
+rcmdcheck::rcmdcheck(
+    args = c(
+        "--no-build-vignettes",
+        "--no-manual",
+        "--no-vignettes",
+        "--timings"
+    ),
+    build_args = c(
+        "--no-build-vignettes",
+        "--no-manual"
+    )
+)
 BiocCheck::BiocCheck(`quit-with-status` = TRUE)
 lintr::lint_package()
