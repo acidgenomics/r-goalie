2b60c939f45c1e5ea374b93da9ece37efe78d4f8
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-15 11:23:35 -0400

    Update unit tests

--- tests/testthat/test-check-scalar-matchesUniqueGeneNames.R
+++ tests/testthat/test-check-scalar-matchesUniqueGeneNames.R
@@ -16,13 +16,34 @@ se <- SummarizedExperiment(
     )
 )
 
+genes <- rowData(se)[["geneName"]]
+
 test_that("TRUE", {
-    genes <- rowData(se)[["geneName"]]
     ok <- matchesUniqueGeneNames(x = se, genes = genes)
     expect_true(ok)
 })
 
-test_that("FALSE", {
+test_that("Not an S4 object", {
+    ok <- matchesUniqueGeneNames(x = assay(se), genes = genes)
+    expect_false(ok)
+    expect_s3_class(ok, "goalie")
+    expect_identical(
+        cause(ok),
+        noquote("Not an S4 class object.")
+    )
+})
+
+test_that("Invalid genes input", {
+    ok <- matchesUniqueGeneNames(x = se, genes = NULL)
+    expect_false(ok)
+    expect_s3_class(ok, "goalie")
+    expect_identical(
+        cause(ok),
+        noquote("genes is not character.")
+    )
+})
+
+test_that("Non-unique gene names", {
     rowData(se)[["geneName"]][[2L]] <- rowData(se)[["geneName"]][[1L]]
     genes <- rowData(se)[["geneName"]]
     ok <- matchesUniqueGeneNames(x = se, genes = genes)
@@ -31,5 +52,26 @@ test_that("FALSE", {
     expect_identical(
         cause(ok),
         noquote("Non-unique gene names: SYMBOL1")
+    )
+})
+
+test_that("No gene names defined in object", {
+    rowData(se)[["geneName"]] <- NULL
+    ok <- matchesUniqueGeneNames(x = se, genes = genes)
+    expect_false(ok)
+    expect_s3_class(ok, "goalie")
+    expect_identical(
+        cause(ok),
+        noquote("Gene names are not defined in object.")
+    )
+})
+
+test_that("User-requested genes that aren't defined", {
+    ok <- matchesUniqueGeneNames(x = se, genes = c("XXXXXX", "YYYYYY"))
+    expect_false(ok)
+    expect_s3_class(ok, "goalie")
+    expect_identical(
+        cause(ok),
+        noquote("Genes missing: XXXXXX, YYYYYY")
     )
 })
