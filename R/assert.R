45c6a8e637f24dd2b4f96bb1441301f274c5157a
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-15 11:24:01 -0400

    Improve assert engine, using R 3.6 conventions

--- R/assert.R
+++ R/assert.R
@@ -3,6 +3,10 @@
 #' [assert()] is a drop-in replacement for [`stopifnot()`][base::stopifnot]
 #' supporting more informative error messages.
 #'
+#' If any of the expressions defined in `...` are not [`all`](base::all) `TRUE`,
+#' [`stop`][base::stop] is called, producing an error message indicating the
+#' first expression which was not `TRUE`.
+#'
 #' @export
 #'
 #' @param ... Any number of R expressions that return `logical(1)`, each of
@@ -34,67 +38,55 @@ assert <- function(
     msg = NULL,
     traceback = getOption("acid.traceback", default = FALSE)
 ) {
-    # Note that we're using `i` along with `...elt()` here to eval the call.
+    n <- ...length()
+    if (n == 0L) {
+        stop("No assert check defined.")
+    }
     dots <- as.call(substitute(...()))
-    for (i in seq_along(dots)) {
-        call <- dots[[i]]
-        res <- withCallingHandlers(
-            expr = tryCatch(
-                expr = ...elt(i),
-                error = function(e) {
-                    e[["call"]] <- call
-                    stop(e)
-                }
-            ),
-            warning = function(w) {
-                w[["call"]] <- call
-                w
-            }
-        )
 
-        # Ensure that all check functions return boolean.
-        # This behavior differs from `stopifnot()` and is more consistent.
-        if (!(is.logical(res) && length(res) == 1L)) {
-            stop(paste(
-                "Assert check failure.\n",
-                "Check did not return a boolean flag (TRUE/FALSE).",
-                .Dparse(call),
-                sep = "\n"
+    for (i in seq_len(n)) {
+        r <- ...elt(i)
+
+        if (!(is.logical(r) && length(r) == 1L)) {
+            stop(sprintf(
+                paste0(
+                    "Assert failure.\n",
+                    "Check did not return a boolean flag (TRUE/FALSE).\n",
+                    "[%s]: %s"
+                ),
+                i, .Dparse(dots[[i]])
             ))
-        } else {
-            # Ensure we're stripping names off of logical. Otherwise, `isTRUE()`
-            # check will fail on R 3.4.
-            res <- unname(res)
+        } else if (identical(r, TRUE)) {
+            next
         }
 
-        # Stop on the first assert check failure.
-        if (!isTRUE(res)) {
-            # Note that we're allowing the user to define the message.
-            if (!isString(msg)) {
-                # Always return a `stopifnot()`-like error.
-                msg <- c(
-                    "Assert failure.",
-                    sprintf("%s is not TRUE.", .Dparse(call))
-                )
-                # Check for defined cause attribute.
-                cause <- cause(res)
-                if (!is.null(cause)) {
-                    # Capturing the S3 print method on goalie class here.
-                    msg <- c(msg, capture.output(print(res))[-1L])
-                }
-                msg <- paste0(msg, collapse = "\n")
+        # Note that we're allowing the user to define the message.
+        if (!isString(msg)) {
+            # Always return a `stopifnot()`-like error.
+            msg <- c(
+                "Assert failure.",
+                sprintf("[%s] %s is not TRUE.", i, .Dparse(dots[[i]]))
+            )
+            # Check for defined cause attribute.
+            cause <- cause(r)
+            if (!is.null(cause)) {
+                # Capturing the S3 print method on goalie class here.
+                msg <- c(msg, capture.output(print(r))[-1L])
             }
-            # Include the traceback in error by default.
-            if (isTRUE(traceback)) {
-                # Note that we're reversing the call stack here to make it
-                # easier to see the parents.
-                stack <- rev(sys.calls())
-                stack <- printString(stack)
-                # Add the traceback to the error message.
-                msg <- paste(msg, "Traceback:", stack, sep = "\n")
-            }
-            stop(simpleError(msg, call = sys.call(-1L)))
+            msg <- paste0(msg, collapse = "\n")
         }
+
+        # Include the traceback in error.
+        if (isTRUE(traceback)) {
+            # Note that we're reversing the call stack here to make it
+            # easier to see the parents.
+            stack <- rev(sys.calls())
+            stack <- printString(stack)
+            # Add the traceback to the error message.
+            msg <- paste(msg, "Traceback:", stack, sep = "\n")
+        }
+
+        stop(simpleError(msg, call = if (p <- sys.parent(1L)) sys.call(p)))
     }
 
     invisible(TRUE)
