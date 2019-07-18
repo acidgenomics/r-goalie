35aebf4f127db0f9bac43d8995adbd5385f873d8
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-18 08:55:26 -0400

    Prepare release

--- R/deprecated.R
+++ R/deprecated.R
@@ -4,32 +4,57 @@
 
 
 #' Deprecated functions
+#'
 #' @name deprecated
 #' @keywords internal
 #' @inheritParams params
+#' @seealso [Deprecated][base::Deprecated].
 #' @return `.Deprecated`.
+#'
+#' @examples
+#' a <- function(...) {
+#'     .Deprecated("b")
+#'     b(...)
+#' }
+#'
+#' b <- function(x) {
+#'     x + 1L
+#' }
+#'
+#' withCallingHandlers(
+#'     expr = a(1L),
+#'     warning = function(w) {
+#'         print(w)
+#'         invokeRestart("muffleWarning")
+#'     }
+#' )
 NULL
 
 
 
 #' Defunct functions
+#'
 #' @name defunct
 #' @keywords internal
 #' @inheritParams params
+#' @seealso [Defunct][base::Defunct].
 #' @return `.Defunct`.
+#'
+#' @examples
+#' a <- function(...) {
+#'     .Defunct("b")
+#' }
+#'
+#' withCallingHandlers(
+#'     expr = tryCatch(
+#'         expr = a(1L),
+#'         error = function(e) {
+#'             print(e)
+#'             invisible()
+#'         }
+#'     )
+#' )
 NULL
-
-
-
-# v0.2.0 =======================================================================
-# areDirs/checkDirectoryExists
-# areFiles/checkFileExists
-# areSamplesUnique
-# areURLs
-# areUniqueGeneNames
-# isAnImplicitInteger (isInt)
-# isImplicitInteger (isIntegerish)
-# validDimnames (hasValidDimnames)
 
 
 
