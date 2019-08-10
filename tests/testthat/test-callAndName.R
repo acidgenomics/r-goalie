4b5c48d2a082e6a432a9f8cbd84e751e586b9650
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-10 16:43:31 -0400

    Pass lint checks

--- tests/testthat/test-callAndName.R
+++ tests/testthat/test-callAndName.R
@@ -2,7 +2,7 @@ context("callAndName")
 
 test_that("callAndName", {
     expect_identical(
-        callAndName(is.finite, c(1, Inf, NA)),
+        callAndName(is.finite, c(1, Inf, NA)),  # nolint
         c(
             "1.000000000000000e+00" = TRUE,
             "Inf" = FALSE,
