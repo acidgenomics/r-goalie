a7326decd7ec1c86d69b349226c5e113105fde29
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-07 15:36:38 -0400

    Add code coverage

--- tests/testthat/test-check-scalar-hasGitHubPAT.R
+++ tests/testthat/test-check-scalar-hasGitHubPAT.R
@@ -1,6 +1,7 @@
 context("check : scalar : hasGitHubPAT")
 
-test_that("hasGitHubPAT", {
+test_that("GITHUB_PAT environment variable", {
+    Sys.setenv("GITHUB_PAT" = "XXX")
     x <- hasGitHubPAT()
-    expect_type(x, "logical")
+    expect_true(x)
 })
