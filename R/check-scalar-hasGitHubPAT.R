c090361f7bb09217dd321f6bcf3ed5e694786345
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-07 15:34:18 -0400

    Add GitHub PAT check

---
+++ R/check-scalar-hasGitHubPAT.R
@@ -0,0 +1,20 @@
+#' Does the current session have a GitHub personal access token?
+#'
+#' Required for package installs from GitHub, otherwise will hit rate limit.
+#'
+#' @name check-scalar-hasGitHubPAT
+#' @note Updated 2019-10-04.
+#'
+#' @inherit check return
+#'
+#' @examples
+#' hasGitHubPAT()
+NULL
+
+
+
+#' @rdname check-scalar-hasGitHubPAT
+#' @export
+hasGitHubPAT <- function() {
+    isTRUE(nzchar(Sys.getenv("GITHUB_PAT")))
+}
