a03796aa06465005e18d4962b719255544292e01
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-07 17:00:15 -0400

    Migrate `isInstalled` from koopa here

---
+++ R/check-vector-isInstalled.R
@@ -0,0 +1,38 @@
+#' Is the package installed?
+#'
+#' @name check-vector-isInstalled
+#' @note Updated 2020-04-07.
+#'
+#' @inherit check return
+#'
+#' @examples
+#' ## TRUE ====
+#' isInstalled(c("base", "utils"))
+#'
+#' ## FALSE ====
+#' isInstalled(c("AAA", "BBB"))
+NULL
+
+
+
+## Vector ======================================================================
+#' @describeIn check-vector-isInstalled Vectorized.
+#' @export
+isInstalled <- function(x) {
+    stopifnot(requireNamespace("utils", quietly = TRUE))
+    # Note that GitHub packages are "user/repo", so use basename.
+    ok <- basename(x) %in% rownames(installed.packages())
+    names(ok) <- toNames(x)
+    setCause(ok, false = "not installed")
+}
+
+
+
+## Scalar ======================================================================
+#' @describeIn check-vector-isInstalled Scalar.
+#' @export
+allAreInstalled <- function(x) {
+    ok <- isInstalled(x)
+    if (!all(ok)) return(falseFromVector(ok))
+    TRUE
+}
