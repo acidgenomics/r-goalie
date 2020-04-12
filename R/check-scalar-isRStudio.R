84619e45d71c03680f300c28ab4d7ed6d868c9d6
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-12 08:53:08 -0400

    Initial commit

---
+++ R/check-scalar-isRStudio.R
@@ -0,0 +1,22 @@
+#' Is the current R session running inside RStudio?
+#'
+#' @name check-scalar-isRStudio
+#' @note Updated 2020-04-12.
+#'
+#' @inherit check return
+#'
+#' @examples
+#' isRStudio()
+NULL
+
+
+
+#' @rdname check-scalar-isRStudio
+#' @export
+isRStudio <- function() {
+    ok <- isTRUE(nzchar(Sys.getenv("RSTUDIO_USER_IDENTITY")))
+    if (!isTRUE(ok)) {
+        return(false("RStudio not detected."))
+    }
+    TRUE
+}
