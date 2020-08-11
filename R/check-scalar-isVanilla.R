f30710aec980ce8126daa36fc44e05630fba5308
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-11 18:49:17 -0400

    Add `isVanilla` check

---
+++ R/check-scalar-isVanilla.R
@@ -0,0 +1,23 @@
+#' Is the R session running as vanilla?
+#'
+#' @name check-scalar-isVanilla
+#' @note Updated 2020-08-11.
+#'
+#' @inherit check return
+#'
+#' @examples
+#' isVanilla()
+NULL
+
+
+
+#' @rdname check-scalar-isVanilla
+#' @export
+isVanilla <-
+    function() {
+        ok <- isSubset("--vanilla", commandArgs())
+        if (!isTRUE(ok)) {
+            return(false("R session is not running with '--vanilla' flag."))
+        }
+        TRUE
+    }
---
+++ tests/testthat/test-check-scalar-isVanilla.R
@@ -0,0 +1,5 @@
+context("check : scalar : isVanilla")
+
+test_that("isVanilla", {
+    expect_type(isVanilla(), "logical")
+})
