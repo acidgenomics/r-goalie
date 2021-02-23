94517802c9dd8ebf5b976b6a1fe79075962101a1
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-23 12:53:48 -0500

    Create the main `goalie` generator

--- R/AllGenerators.R
+++ R/AllGenerators.R
@@ -6,21 +6,21 @@
 #' @export
 #' @note Updated 2021-02-23.
 #'
-#' @param x `logical`.
+#' @param object `logical`.
 #' @param cause `character(0-1)`.
 #'
 #' @return `goalie`, which extends `logical`.
 #'
 #' @examples
 #' x <- goalie(
-#'     x = c(FALSE, TRUE),
+#'     object = c(FALSE, TRUE),
 #'     cause = "Element 1 is false."
 #' )
-#' print(x)
-goalie <- function(x, cause = character()) {
+#' print(object)
+goalie <- function(object, cause = character()) {
     stopifnot(
-        is.logical(x),
-        is.character(cause) && length(cause) <= 1L
+        is.logical(object),
+        is.character(cause)
     )
-    new(Class = "goalie", x)
+    new(Class = "goalie", ".Data" = object, "cause" = cause)
 }
