5b4463be5bb089555c6a5a45c1ff5812199a9b79
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-23 15:12:04 -0500

    Simplify the assert engine approach

--- R/fixme-assert.R
+++ R/fixme-assert.R
@@ -1,8 +1,3 @@
-## FIXME THIS IS CAUSING THE GOALIE PRINT METHOD, WHICH HAS ISSUES WITH R MARKDOWN.
-## FIXME NEED TO RETURN SCALAR CAUSE FROM S4 CLASS.
-
-
-
 #' Assert that certain conditions are true
 #'
 #' [assert()] is a drop-in replacement for [`stopifnot()`][base::stopifnot]
@@ -12,7 +7,7 @@
 #' [`stop`][base::stop] is called, producing an error message indicating the
 #' first expression which was not `TRUE`.
 #'
-#' @name engine-assert
+#' @export
 #' @note Updated 2021-02-23.
 #'
 #' @inheritParams AcidRoxygen::params
@@ -34,12 +29,6 @@
 #'     is.atomic("example"),
 #'     is.character("example")
 #' )
-NULL
-
-
-
-#' @rdname engine-assert
-#' @export
 assert <- function(..., msg = NULL) {
     n <- ...length()
     if (identical(n, 0L)) {
@@ -65,17 +54,13 @@ assert <- function(..., msg = NULL) {
             next
         }
         ## Note that we're allowing the user to define the message.
-        if (!isString(msg)) {
-            ## Always return a `stopifnot()`-like error.
+        if (is.null(msg)) {
             msg <- sprintf("Assert failure.\n[%s] %s is not TRUE.", i, call)
-            ## Check for defined cause attribute.
-            cause <- cause(r)
-            if (!is.null(cause)) {
-                ## Capturing the S3 print method on goalie class here.
-                ## FIXME RETHINK THIS APPROACH...ISSUES WITH R MARKDOWN?
-                msg <- c(msg, capture.output(print(r))[-1L])
+            if (is(r, "goalie")) {
+                cause <- cause(r)
+                stopifnot(is.character(cause) && length(cause) == 1L)
+                msg <- paste(msg, cause)
             }
-            msg <- paste0(msg, collapse = "\n")
         }
         stop(simpleError(msg, call = if (p <- sys.parent(1L)) sys.call(p)))
     }
