95c99123513d8c7ffa2eda32fac384bbcb5f7ab4
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-23 15:37:58 -0500

    Work toward stable release

--- R/false.R
+++ R/false.R
@@ -8,56 +8,10 @@
 #' @return `goalie`.
 #'
 #' @examples
-#' ## false ====
 #' x <- false("'%s' is invalid.", "xxx")
 #' print(x)
 #' print(cause(x))
-#'
-#' ## falseFromVector ====
-#' x <- goalie(
-#'     object = c("aaa" = FALSE, "bbb" = TRUE, "ccc" = FALSE),
-#'     cause = c("xxx", NA, "yyy")
-#' )
-#' print(x)
-#' print(cause(x))
-#' xx <- falseFromVector(x)
-#' print(xx)
-#' print(cause(xx))
 false <- function(...) {
     stopifnot(isTRUE(nargs() > 0L))
     goalie(object = FALSE, cause = sprintf(...))
 }
-
-
-
-## NOTE Subset operation on goalie currently coerces back to logical.
-#' @rdname false
-#' @export
-## Updated 2021-02-23.
-`falseFromVector,goalie` <-  # nolint
-    function(x) {
-        cause <- cause(x)[which(x == FALSE)]
-        stopifnot(!is.null(names(cause)))
-        x <- mapply(
-            name = names(cause),
-            value = cause,
-            FUN = function(name, value) {
-                paste0(name, ": ", value)
-            },
-            SIMPLIFY = FALSE,
-            USE.NAMES = FALSE
-        )
-        x <- unlist(x)
-        x <- paste(x, collapse = "; ")
-        false(x)
-    }
-
-
-
-#' @rdname false
-#' @export
-setMethod(
-    f = "falseFromVector",
-    signature = signature("goalie"),
-    definition = `falseFromVector,goalie`
-)
