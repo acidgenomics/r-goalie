3859a4f8ec93d628a77a59ded322249d28a6667d
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-23 12:53:37 -0500

    Rework goalie object as S4 with class definition and validity check

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,11 +1,8 @@
-## FIXME cause slot should only allow character(1) or NULL.
-
-
-
 #' goalie check
 #'
 #' @details
-#' Contains a `logical` with `cause` if any elements are FALSE.
+#' Contains a `logical` with `cause` of `character(1)` if any elements are
+#' `FALSE`.
 #'
 #' @export
 #' @note Updated 2021-02-23.
@@ -21,6 +18,19 @@ setClass(
 setValidity(
     Class = "goalie",
     method = function(object) {
+        if (!isTRUE(length(object) > 0)) {
+            return("Object is 'logical(0)'.")
+        }
+        cause <- slot(object, name = "cause")
+        if (isTRUE(all(object))) {
+            if (!identical(cause, character())) {
+                return("'cause' attribute is not allowed for TRUE return.")
+            }
+        } else {
+            if (!isTRUE(is.character(cause) && length(cause) == 1L)) {
+                return("'cause' attribute is not 'character(1)'.")
+            }
+        }
         TRUE
     }
 )
