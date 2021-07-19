019571d929ec00f6ced246e5de905cea7ab8866d
Michael Steinbaugh <mike@steinbaugh.com>
2021-07-19 11:36:39 -0400

    Improve coverage

---
+++ tests/testthat/test-engine-internal.R
@@ -0,0 +1,11 @@
+context("internal")
+
+test_that("toNames complex", {
+    object <- complex(length.out = 2L)
+    expect_is(object, "complex")
+    object <- .toNames(object)
+    expect_identical(
+        object = object,
+        expected = c("0+0i", "0+0i")
+    )
+})
