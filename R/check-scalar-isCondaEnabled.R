a55651b61e8f77021570d37d8ea57ac1616665a2
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-19 09:06:14 -0400

    Add conda environment check function

---
+++ R/check-scalar-isCondaEnabled.R
@@ -0,0 +1,62 @@
+#' Is conda enabled (active) in the current R session?
+#'
+#' @name check-scalar-isCondaEnabled
+#' @note Updated 2021-08-19.
+#'
+#' @inherit check return
+#'
+#' @details
+#' This checks `CONDA_DEFAULT_ENV` and `CONDA_SHLVL` system environment
+#' variables internally.
+#'
+#' @param ignoreBase `logical(1)`.
+#'   If `TRUE`, don't consider conda to be "active" if only the base
+#'   environment is loaded. This is useful to avoid some false-positive
+#'   situations when handling the main conda `activate` script in shell
+#'   sessions.
+#'
+#' @examples
+#' vars <- c("CONDA_DEFAULT_ENV", "CONDA_SHLVL")
+#'
+#' ## TRUE ====
+#' Sys.unsetenv(vars)
+#' Sys.setenv(
+#'     "CONDA_DEFAULT_ENV" = "test",
+#'     "CONDA_SHLVL" = "2"
+#' )
+#' isCondaEnabled()
+#'
+#' Sys.unsetenv(vars)
+#' Sys.setenv(
+#'     "CONDA_DEFAULT_ENV" = "base",
+#'     "CONDA_SHLVL" = "1"
+#' )
+#' isCondaEnabled(ignoreBase = FALSE)
+#'
+#' ## FALSE ====
+#' Sys.unsetenv(vars)
+#' isCondaEnabled()
+#'
+#' Sys.unsetenv(vars)
+#' Sys.setenv(
+#'     "CONDA_DEFAULT_ENV" = "base",
+#'     "CONDA_SHLVL" = "1"
+#' )
+#' isCondaEnabled(ignoreBase = TRUE)
+NULL
+
+
+
+#' @rdname check-scalar-isCondaEnabled
+#' @export
+isCondaEnabled <- function(ignoreBase = TRUE) {
+    defaultEnv <- Sys.getenv("CONDA_DEFAULT_ENV")
+    shlvl <- as.integer(Sys.getenv("CONDA_SHLVL"))
+    ## Allow the user to selectively ignore when "base" environment is active.
+    if (isTRUE(ignoreBase)) {
+        ok <- identical(defaultEnv, "base")
+        if (isTRUE(ok)) return(FALSE)
+    }
+    ok <- isString(defaultEnv) && isTRUE(shlvl > 0L)
+    ok
+}
