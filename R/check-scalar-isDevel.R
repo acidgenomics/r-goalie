f78e887a6910eb458a384965a21c15031a25cde8
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-19 10:28:53 -0400

    Exclude platform/configuration-specific checks from CI coverage report

--- R/check-scalar-isDevel.R
+++ R/check-scalar-isDevel.R
@@ -1,3 +1,7 @@
+## nocov start
+
+
+
 #' Is the current session running inside R-devel?
 #'
 #' @name check-scalar-isDevel
@@ -15,10 +19,13 @@ NULL
 #' @export
 isDevel <- function() {
     ok <- isMatchingFixed(x = R.version.string, pattern = "development")
-    ## nocov start
+
     if (!isTRUE(ok)) {
         return(false("Not R-devel: '%s'.", R.version.string))
     }
     TRUE
-    ## nocov end
 }
+
+
+
+## nocov end
