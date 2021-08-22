ccab6193166768f814608b5f12b6c8af662a21d3
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-22 16:22:24 -0400

    Add support for `lib`

--- R/check-vector-isInstalled.R
+++ R/check-vector-isInstalled.R
@@ -1,10 +1,14 @@
 #' Is the package installed?
 #'
 #' @name check-vector-isInstalled
-#' @note Updated 2020-04-08.
+#' @note Updated 2021-08-22.
 #'
 #' @inherit check
 #' @inheritParams AcidRoxygen::params
+#'
+#' @param lib `character(1)` or `NULL`.
+#'   R library location.
+#'   If left `NULL`, includes all known library paths defined in `.libPaths`.
 #'
 #' @examples
 #' ## TRUE ====
@@ -19,9 +23,10 @@ NULL
 ## Vector ======================================================================
 #' @describeIn check-vector-isInstalled Vectorized.
 #' @export
-isInstalled <- function(x) {
+isInstalled <- function(x, lib = NULL) {
+    df <- installed.packages(lib.loc = lib)
     # Note that GitHub packages are "owner/repo", so use basename.
-    ok <- basename(x) %in% rownames(installed.packages())
+    ok <- basename(x) %in% rownames(df)
     names(ok) <- .toNames(x)
     setCause(ok, false = "not installed")
 }
@@ -31,8 +36,8 @@ isInstalled <- function(x) {
 ## Scalar ======================================================================
 #' @describeIn check-vector-isInstalled Scalar.
 #' @export
-allAreInstalled <- function(x) {
-    ok <- isInstalled(x)
+allAreInstalled <- function(x, lib = NULL) {
+    ok <- isInstalled(x, lib = lib)
     if (!all(ok)) return(falseFromVector(ok))
     TRUE
 }
