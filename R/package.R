a35af414081c89754c94e97300e6ea79eea1bee1
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 12:49:45 -0500

    Reorganize imports and package description

--- R/package.R
+++ R/package.R
@@ -1,4 +1,3 @@
-## Package description =========================================================
 #' goalie
 #'
 #' Assertive check functions for defensive R programming.
@@ -6,13 +5,3 @@
 #' @aliases NULL
 #' @keywords internal
 "_PACKAGE"
-
-
-
-## Namespace ===================================================================
-#' @importClassesFrom methods logical
-#'
-#' @importFrom methods as formalArgs is isGeneric new selectMethod setClass
-#'   setGeneric setMethod setValidity slot show signature slot
-#' @importFrom utils capture.output head installed.packages packageVersion
-NULL
