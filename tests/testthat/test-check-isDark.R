cc57e547b03064f83db9276adf3795e9c1b59b3f
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-04 13:08:01 -0500

    Add check for dark mode

---
+++ tests/testthat/test-check-isDark.R
@@ -0,0 +1,21 @@
+context("check : scalar : isDark")
+
+test_that("TRUE", {
+    opt <- getOption("acid.dark")
+    options("acid.dark" = TRUE)
+    ok <- isDark()
+    expect_true(ok)
+    options("acid.dark" = opt)
+})
+
+test_that("FALSE", {
+    opt <- getOption("acid.dark")
+    options("acid.dark" = NULL)
+    ok <- isDark()
+    expect_false(ok)
+    expect_s4_class(ok, "goalie")
+    expect_identical(
+        object = cause(ok),
+        expected = "Dark mode is not enabled."
+    )
+})
