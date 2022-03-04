abe2ce19d69cfdcd6b9059c950e5291a63882121
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-04 08:59:02 -0500

    Add support for checking BiocParallel param

---
+++ R/check-scalar-isBiocParallelParam.R
@@ -0,0 +1,37 @@
+#' Does the input contain a BiocParallel parameter?
+#'
+#' @name check-scalar-isBiocParallelParam
+#' @note Updated 2022-03-04.
+#'
+#' @inherit check
+#' @inheritParams AcidRoxygen::params
+#'
+#' @seealso `BiocParallel::bpparam`.
+#'
+#' @examples
+#' ## TRUE ====
+#' if (isInstalled("BiocParallel")) {
+#'     isBiocParallelParam(BiocParallel::bpparam())
+#' }
+#'
+#' ## FALSE ====
+#' isBiocParallelParam(list())
+NULL
+
+
+
+#' @rdname check-scalar-isBiocParallelParam
+#' @export
+isBiocParallelParam <- function(x, .xname = getNameInParent(x)) {
+    ok <- all(
+        identical(
+            attributes(class(x))[["package"]],
+            "BiocParallel"
+        ),
+        grepl("Param$", class(x))
+    )
+    if (!isTRUE(ok)) {
+        return(false("{.var %s} is not a BiocParallel param.", .xname))
+    }
+    return(TRUE)
+}
