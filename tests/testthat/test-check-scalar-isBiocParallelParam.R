ac97c74ab8f21a232108cf0b29aab907b22d269e
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-04 10:41:51 -0500

    Improve code coverage

---
+++ tests/testthat/test-check-scalar-isBiocParallelParam.R
@@ -0,0 +1,18 @@
+context("check : scalar : isBiocParallelParam")
+
+skip_if_not_installed("BiocParallel")
+
+test_that("TRUE", {
+    ok <- isBiocParallelParam(BiocParallel::bpparam())
+    expect_true(ok)
+})
+
+test_that("FALSE", {
+    ok <- isBiocParallelParam(list())
+    expect_s4_class(ok, "goalie")
+    expect_false(ok)
+    expect_identical(
+        object = cause(ok),
+        expected = "{.var list()} is not a BiocParallel param."
+    )
+})
