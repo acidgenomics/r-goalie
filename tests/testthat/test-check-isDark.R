a7b5f2d2ccf840341e2530304ae8d821dd6217e3
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-13 15:36:53 -0400

    Improve testing against environment variable

--- tests/testthat/test-check-isDark.R
+++ tests/testthat/test-check-isDark.R
@@ -1,19 +1,26 @@
+skip_if_not_installed("withr")
+
 test_that("TRUE", {
-    opt <- getOption("acid.dark")
-    options("acid.dark" = TRUE)
-    ok <- isDark()
-    expect_true(ok)
-    options("acid.dark" = opt)
+    withr::with_options(
+        new = list("acid.dark" = TRUE),
+        code = {
+            ok <- isDark()
+            expect_true(ok)
+        }
+    )
 })
 
 test_that("FALSE", {
-    opt <- getOption("acid.dark")
-    options("acid.dark" = NULL)
-    ok <- isDark()
-    expect_false(ok)
-    expect_s4_class(ok, "goalie")
-    expect_identical(
-        object = cause(ok),
-        expected = "Dark mode is not enabled."
+    withr::with_options(
+        new = list("acid.dark" = NULL),
+        code = {
+            ok <- isDark()
+            expect_false(ok)
+            expect_s4_class(ok, "goalie")
+            expect_identical(
+                object = cause(ok),
+                expected = "Dark mode is not enabled."
+            )
+        }
     )
 })
