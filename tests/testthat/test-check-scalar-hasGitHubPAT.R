d5891612bd0ff38a04cc65cb7c372bd7180b4851
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-13 15:34:52 -0400

    Harden environment testing using withr

--- tests/testthat/test-check-scalar-hasGitHubPAT.R
+++ tests/testthat/test-check-scalar-hasGitHubPAT.R
@@ -1,11 +1,21 @@
+skip_if_not_installed("withr")
+
 test_that("TRUE", {
-    Sys.setenv("GITHUB_PAT" = "XXX")
-    x <- hasGitHubPAT()
-    expect_true(x)
+    withr::with_envvar(
+        new = c("GITHUB_PAT" = "XXX"),
+        code = {
+            ok <- hasGitHubPAT()
+            expect_true(ok)
+        }
+    )
 })
 
 test_that("FALSE", {
-    Sys.setenv("GITHUB_PAT" = "")
-    x <- hasGitHubPAT()
-    expect_false(x)
+    withr::with_envvar(
+        new = c("GITHUB_PAT" = ""),
+        code = {
+            ok <- hasGitHubPAT()
+            expect_false(ok)
+        }
+    )
 })
