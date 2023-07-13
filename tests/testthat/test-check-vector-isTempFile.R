7b5736a91546d235b7d3c784601e35e5c9ce5fe6
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-13 09:52:25 -0400

    Add code coverage

--- tests/testthat/test-check-vector-isTempFile.R
+++ tests/testthat/test-check-vector-isTempFile.R
@@ -2,10 +2,12 @@ test_that("TRUE", {
     x <- tempfile()
     file.create(x)
     expect_true(isATempFile(x))
+    expect_true(allAreTempFiles(x))
     unlink(x)
 })
 
 test_that("FALSE", {
     x <- tempfile()
     expect_false(isATempFile(x))
+    expect_false(allAreTempFiles(x))
 })
