80cb93ce7f8069b8493631c497c71eacd4a748b7
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-21 16:11:18 -0400

    Add code coverage

---
+++ tests/testthat/test-check-scalar-isTximport.R
@@ -0,0 +1,26 @@
+test_that("TRUE", {
+    skip_if_not_installed("tximport")
+    skip_if_not_installed("tximportData")
+    dir <- system.file("extdata", package = "tximportData")
+    samples <- read.table(file.path(dir, "samples.txt"), header = TRUE)
+    files <- file.path(dir, "salmon", samples[["run"]], "quant.sf.gz")
+    names(files) <- paste0("sample", seq(from = 1L, to = length(files)))
+    object <- tximport::tximport(
+        files = files,
+        type = "salmon",
+        txIn = TRUE,
+        txOut = TRUE
+    )
+    ok <- isTximport(object)
+    expect_true(ok)
+})
+
+test_that("FALSE", {
+    ok <- isTximport(list())
+    expect_s4_class(ok, "goalie")
+    expect_false(ok)
+    expect_identical(
+        object = cause(ok),
+        expected = "{.var list()} is not a tximport list."
+    )
+})
