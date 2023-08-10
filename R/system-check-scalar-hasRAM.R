1ca62b63eb15cb8fe608456ecf88992471ce3107
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-10 15:19:21 -0400

    Add hasRAM assert check

---
+++ R/system-check-scalar-hasRAM.R
@@ -0,0 +1,38 @@
+#' Does the current machine have a sufficient RAM?
+#'
+#' @name check-scalar-hasRAM
+#' @note Updated 2023-08-10.
+#'
+#' @inherit check return
+#'
+#' @param n `integer(1)`.
+#' Minimum amount of RAM in gigabytes (GB).
+#'
+#' @examples
+#' ## TRUE ====
+#' hasRAM(n = 1L)
+#'
+#' ## FALSE ====
+#' hasRAM(n = Inf)
+NULL
+
+
+
+#' @rdname check-scalar-hasRAM
+#' @export
+hasRAM <- function(n) {
+    assert(
+        isInt(n),
+        requireNamespaces("AcidBase")
+    )
+    ram <- AcidBase::ram()
+    ok <- cpus >= n
+    if (!isTRUE(ok)) {
+        return(false(
+            "Not enough RAM (in GB): %s (%s) < %s (%s).",
+            as.character(ram), "current",
+            as.character(n), "expected"
+        ))
+    }
+    TRUE
+}
