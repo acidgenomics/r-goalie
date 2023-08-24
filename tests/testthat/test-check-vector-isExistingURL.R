76110c4df3e57e64420f71b35aeffa4b8fa297dc
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-24 12:53:15 -0400

    Improve URL check code coverage

--- tests/testthat/test-check-vector-isExistingURL.R
+++ tests/testthat/test-check-vector-isExistingURL.R
@@ -1,29 +1,31 @@
 urls <- c("https://www.r-project.org/", "ftp://r-project.org/")
 
-test_that("isURL", {
-    ok <- isURL(urls)
+test_that("isExistingURL", {
+    ok <- isExistingURL(urls)
     expect_true(all(ok))
-    ok <- isURL("xxx")
+    ok <- isExistingURL("https://failwhale.acidgenomics.com/")
     expect_s4_class(ok, "goalie")
     expect_false(ok)
     expect_identical(
         object = cause(ok),
-        expected = c("xxx" = "not URL")
+        expected = c(
+            "\"https://failwhale.acidgenomics.com/\"" = "URL doesn't exist"
+        )
     )
 })
 
 test_that("URL connection support", {
     x <- url(urls[[1L]])
     expect_s3_class(x, "url")
-    ok <- isAURL(x)
+    ok <- isAnExistingURL(x)
     expect_true(ok)
     close(x)
 })
 
-test_that("isAURL", {
-    ok <- isAURL(urls[[1L]])
+test_that("isAnExistingURL", {
+    ok <- isAnExistingURL(urls[[1L]])
     expect_true(ok)
-    ok <- isAURL(urls)
+    ok <- isAnExistingURL(urls)
     expect_s4_class(ok, "goalie")
     expect_false(ok)
     expect_identical(
@@ -32,16 +34,7 @@ test_that("isAURL", {
     )
 })
 
-test_that("allAreURLs", {
-    ok <- allAreURLs(urls)
+test_that("allAreExistingURLs", {
+    ok <- allAreExistingURLs(urls)
     expect_true(ok)
-})
-
-test_that("Encoding", {
-    url <- "https://rest.ensembl.org/info/assembly/Homo%20sapiens"
-    ok <- isAURL(url)
-    expect_true(ok)
-    url <- "https://rest.ensembl.org/info/assembly/Homo sapiens"
-    ok <- isAURL(url)
-    expect_false(ok)
 })
