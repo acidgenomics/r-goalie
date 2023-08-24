572e4ef1b121f70b0769a11f7940df7c5a8942f0
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-24 12:26:43 -0400

    Rework URL checks

--- tests/testthat/test-check-vector-isURL.R
+++ tests/testthat/test-check-vector-isURL.R
@@ -1,9 +1,8 @@
 urls <- c("https://www.r-project.org", "ftp://r-project.org")
 
 test_that("isURL", {
-    isURL(urls)
-    isAURL(urls[[1L]])
-    allAreURLs(urls)
+    ok <- isURL(urls)
+    expect_true(all(ok))
     ok <- isURL("xxx")
     expect_s4_class(ok, "goalie")
     expect_false(ok)
@@ -16,12 +15,14 @@ test_that("isURL", {
 test_that("URL connection support", {
     x <- url(urls[[1L]])
     expect_s3_class(x, "url")
-    expect_true(isAURL(x))
+    ok <- isAURL(x)
+    expect_true(ok)
     close(x)
 })
 
 test_that("isAURL", {
-    expect_true(isAURL(urls[[1L]]))
+    ok <- isAURL(urls[[1L]])
+    expect_true(ok)
     ok <- isAURL(urls)
     expect_s4_class(ok, "goalie")
     expect_false(ok)
@@ -32,12 +33,15 @@ test_that("isAURL", {
 })
 
 test_that("allAreURLs", {
-    expect_true(allAreURLs(urls))
+    ok <- allAreURLs(urls)
+    expect_true(ok)
 })
 
 test_that("Encoding", {
     url <- "https://rest.ensembl.org/info/assembly/Homo%20sapiens"
-    expect_true(isAURL(url))
+    ok <- isAURL(url)
+    expect_true(ok)
     url <- "https://rest.ensembl.org/info/assembly/Homo sapiens"
-    expect_false(isAURL(url))
+    ok <- isAURL(url)
+    expect_false(ok)
 })
