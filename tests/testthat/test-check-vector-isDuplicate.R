754ed41c58f52bdec7a6267c06ab5f52d34344a2
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-19 07:00:56 -0400

    Improve coverage

--- tests/testthat/test-check-vector-isDuplicate.R
+++ tests/testthat/test-check-vector-isDuplicate.R
@@ -1,13 +1,17 @@
 test_that("isDuplicate", {
     expect_identical(
         object = isDuplicate(c("a", "a")),
-        expected = c("a" = TRUE, "a" = TRUE)
+        expected = c(TRUE, TRUE)
     )
     ok <- isDuplicate(c("a", "a", "b", "b", "c", "d", NA, NA))
     expect_s4_class(ok, "goalie")
     expect_identical(
         object = as.logical(ok),
         expected = c(TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE)
+    )
+    expect_identical(
+        object = cause(ok),
+        expected = c(NA, NA, NA, NA, "unique", "unique", NA, NA)
     )
     ok <- isDuplicate(c("a", "b", "c"))
     expect_s4_class(ok, "goalie")
