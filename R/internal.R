d2253a5a857966bb0d170a1638cdba9ec2896573
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-29 14:51:46 -0400

    Return empty character on non-atomic input

--- R/internal.R
+++ R/internal.R
@@ -196,7 +196,7 @@ NULL
 #' .toNames(c("sample-1", "hello world"))
 .toNames <- function(x) {
     if (!is.atomic(x)) {
-        return("x")
+        return(character())
     }
     if (is.double(x)) {
         x <- ifelse(
