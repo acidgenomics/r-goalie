15f750f34720b82325782b5e5a0dc440969630bd
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-29 13:55:14 -0400

    Return FALSE if Annotated not passed in

--- R/check-scalar-hasSubset.R
+++ R/check-scalar-hasSubset.R
@@ -1,7 +1,7 @@
 #' Does the object contain a subset of data?
 #'
 #' @name check-scalar-hasSubset
-#' @note Updated 2021-01-04.
+#' @note Updated 2023-09-29.
 #'
 #' @inherit check
 #' @inheritParams AcidRoxygen::params
@@ -27,14 +27,16 @@ hasSubset <-
     function(x,
              metadata = "subset",
              .xname = getNameInParent(x)) {
-        ## FIXME Need to return with cause here.
         if (!is(x, "Annotated")) {
-            return(FALSE)
+            return(false(
+                "{.var %s} is not {.cls %s} class.",
+                .xname, "Annotated"
+            ))
         }
         requireNamespaces("S4Vectors")
         m1 <- metadata
         m2 <- names(S4Vectors::metadata(x))
-        ok <- isSubset(m1, m2)
+        ok <- isSubset(x = m1, y = m2)
         if (!isTRUE(ok)) {
             return(false(
                 "{.var %s} doesn't contain slots in {.fun %s}: %s.",
