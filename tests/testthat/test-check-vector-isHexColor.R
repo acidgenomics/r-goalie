da053c45e0d87e329f66ef838edb8792886d92c6
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-02 10:15:38 -0400

    Update checks

--- tests/testthat/test-check-vector-isHexColor.R
+++ tests/testthat/test-check-vector-isHexColor.R
@@ -1,19 +1,18 @@
 test_that("TRUE", {
+    skip_if_not_installed("viridis")
     x <- viridis::viridis(n = 2L)
     ok <- isHexColor(x)
-    expect_identical(
-        object = ok,
-        expected = c("#440154FF" = TRUE, "#FDE725FF" = TRUE)
-    )
+    expect_identical(ok, rep(TRUE, 2L))
 })
 
 test_that("FALSE", {
+    skip_if_not_installed("ggplot2")
     x <- ggplot2::scale_color_manual
     ok <- isHexColor(x)
     expect_s4_class(ok, "goalie")
     expect_identical(
         object = cause(ok),
-        expected = "{.var x} is not character."
+        expected = c("function" = "not character")
     )
     expect_false(ok)
 })
