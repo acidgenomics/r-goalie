ef30ab7dc86865512fd5eb8e4a64f988d7bc6fe2
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-02 10:52:01 -0400

    Improve our engine checks

--- tests/testthat/test-engine-goalie.R
+++ tests/testthat/test-engine-goalie.R
@@ -1,20 +1,43 @@
-test_that("Multiple FALSE", {
-    ok <- c(FALSE, FALSE)
-    cause <- c("AAA", "BBB")
-    object <- goalie(object = ok, cause = cause)
-    expect_s4_class(object, "goalie")
+test_that("All TRUE", {
     expect_identical(
-        object = cause(object),
-        expected = cause
+        object = goalie(TRUE),
+        expected = TRUE
+    )
+    expect_identical(
+        object = goalie(rep(TRUE, 2L)),
+        expected = rep(TRUE, 2L)
     )
 })
 
-test_that("Multiple TRUE", {
-    ok <- c(TRUE, TRUE)
-    object <- goalie(object = ok)
+test_that("Any FALSE", {
+    expect_error(
+        object = goalie(
+            object = c(FALSE, FALSE),
+            cause = c("AAA", "BBB")
+        ),
+        regexp = "Cause attribute doesn't have names assigned."
+    )
+    object <- goalie(
+        object = c(FALSE, FALSE),
+        cause = c("1" = "AAA", "2" = "BBB")
+    )
     expect_s4_class(object, "goalie")
     expect_identical(
         object = cause(object),
-        expected = rep(x = NA_character_, times = length(object))
+        expected = c("1" = "AAA", "2" = "BBB")
+    )
+})
+
+test_that("Invalid input", {
+    expect_error(
+        object = goalie(logical()),
+        regexp = "Invalid input."
+    )
+    expect_error(
+        object = goalie(
+            object = c(FALSE, TRUE, NA),
+            cause = c("1" = "aaa", "2" = NA_character_, "3" = "ccc")
+        ),
+        regexp = "Object contains NA."
     )
 })
